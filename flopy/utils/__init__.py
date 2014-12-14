1654150d5e727759c3743ac407547ef988eeb792
Mark Bakker <markbak@gmail.com>
2014-12-14 11:47:50 -0800

    removed mfreadbinaries and mtreadbinaries

--- flopy/utils/__init__.py
+++ flopy/utils/__init__.py
@@ -1,5 +1,4 @@
-from mfreadbinaries import ModflowCbcRead, ModflowHdsRead, Mt3dUcnRead
-from mtreadbinaries import Mt3dPsRead
+
 from mfreadnam import parsenamefile
 from util_array import util_3d, util_2d
 from binaryfile import binaryheader, HeadFile, UcnFile, CellBudgetFile
