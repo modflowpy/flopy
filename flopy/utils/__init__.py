b9d821724378c491b93ef3fe1f4b1a3e8e4ec156
Jeremy White <jtwhite1000@gmail.com>
2014-12-17 19:58:09 -0600

    doctoring for util.__init__()

--- flopy/utils/__init__.py
+++ flopy/utils/__init__.py
@@ -1,4 +1,25 @@
+"""
+    the main entry point of utils
 
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+
+    See Also
+    --------
+
+    Notes
+    -----
+
+    Examples
+    --------
+
+    """
 from mfreadnam import parsenamefile
 from util_array import util_3d, util_2d
 from util_list import mflist
