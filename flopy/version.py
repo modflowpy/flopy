68868729347f7e8ce1dcbccf594c759fb2e58fb0
unknown <jdhughes@IGSAAAHMLTW272V.gs.doi.net>
2015-02-15 14:05:01 -0800

    version number update

--- flopy/version.py
+++ flopy/version.py
@@ -1,4 +1,4 @@
 #flopy version file automatically created using...updateversion.pyc
-#            created on......February 15, 2015 14:02:54
-__version__='3.0.3'
-__build__='3.0.3.399'
+#            created on......February 15, 2015 14:04:58
+__version__='3.0.2'
+__build__='3.0.2.400'
