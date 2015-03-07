Bug fix in vectors.  Added masking for plot_array.

flopy/plot/map.py
@@ -110,7 +110,7 @@ class ModelMap(object):
 
         return
 
-    def plot_array(self, a, **kwargs):
+    def plot_array(self, a, masked_values=None, **kwargs):
         """
         Plot an array.  If the array is three-dimensional, then the method
         will plot the layer tied to this class (self.layer).
@@ -119,6 +119,8 @@ class ModelMap(object):
         ----------
         a : numpy.ndarray
             Array to plot.
+        masked_values : iterable of floats, ints
+            Values to mask.
         **kwargs : dictionary
             keyword arguments passed to matplotlib.pyplot.pcolormesh
 
@@ -132,6 +134,9 @@ class ModelMap(object):
             plotarray = a
         else:
             raise Exception('Array must be of dimension 2 or 3')
+        if masked_values is not None:
+            for mval in masked_values:
+                plotarray = np.ma.masked_equal(plotarray, mval)
         quadmesh = plt.pcolormesh(self.xgrid, self.ygrid, plotarray, **kwargs)
         return quadmesh
 
@@ -285,8 +290,12 @@ class ModelMap(object):
 
         x = self.xcentergrid[::istep, ::jstep]
         y = self.ycentergrid[::istep, ::jstep]
-        u = qx[self.layer, ::istep, ::jstep]
-        v = qy[self.layer, ::istep, ::jstep]
+        u = qx[self.layer, :, :]
+        v = qy[self.layer, :, :]
+        u = u[::istep, ::jstep]
+        v = v[::istep, ::jstep]
+        print u.min(), u.max(), qx.min(), qx.max(), frf.min(), frf.max()
+        print v.min(), v.max(), qy.min(), qy.max(), fff.min(), fff.max()
         quiver = self.ax.quiver(x, y, u, v, **kwargs)
 
         return quiver

flopy/plot/plotutil.py
@@ -226,6 +226,7 @@ def centered_specific_discharge(Qx, Qy, Qz, delr, delc, botm):
     NOT FINISHED YET!  STILL NEED TO
     1.  CALCULATE QZ
     2.  DIVIDE qx AND qy BY THE SATURATED THICKNESS
+    3.  ROTATE FOR MAP PRESENTATION
 
     """
     qx = None
@@ -250,7 +251,7 @@ def centered_specific_discharge(Qx, Qy, Qz, delr, delc, botm):
         qy = np.empty(Qy.shape, dtype=Qy.dtype)
 
         for k in xrange(nlay):
-            for i in xrange(ncol):
+            for i in xrange(nrow):
                 qy[k, i, :] = Qy[k, i, :] / delr[:]
 
         qy[:, 1:, :] = 0.5 * (qy[:, 0:nrow-1, :] + qx[:, 1:nrow, :])