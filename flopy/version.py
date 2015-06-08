b534b67b11ef659ff5373325c0732fca0e837894
Joseph D Hughes <jdhughes@usgs.gov>
2015-06-08 10:08:38 -0400

    version number update

--- flopy/version.py
+++ flopy/version.py
@@ -1,4 +1,4 @@
 #flopy version file automatically created using...updateversion.pyc
-#            created on......June 08, 2015 10:06:08
+#            created on......June 08, 2015 10:08:38
 __version__='3.2.1'
-__build__='3.2.1.603'
+__build__='3.2.1.604'
