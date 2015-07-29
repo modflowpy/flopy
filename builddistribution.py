684da0ef9de1a9e0422872c892f858f0d63141c0
Joseph D Hughes <jdhughes@usgs.gov>
2015-07-29 14:01:00 -0400

    Revised setup.py to remove requirements file dependency. This created problems since this was difficult to automatically include in the source zip file.
    Revised builddistribution.py and updateversion.py to allow command line arguments to control version number updates. Now have more control over build and when new versions are registered with PyPI.

--- builddistribution.py
+++ builddistribution.py
@@ -5,22 +5,56 @@ import subprocess
 
 from updateversion import update_version
 
-#--determine if the version needs to be updated
-update_version()
+def build_distribution(upd=False, utag=False, reg=False, winbuild=False, 
+                       major=False, minor=False):
+    #--determine if the version needs to be updated
+    if upd:
+        update_version(utag, major, minor)
+    
+    #--install the source
+    subprocess.call(['python', 'setup.py', 'install'])
+    
+    #--create the source distribution
+    subprocess.call(['python', 'setup.py', 'sdist', '--format=zip'])
+    
+    #--create 32-bit and 64-bit windows installers
+    if winbuild:
+        if 'windows' in platform.system().lower():
+            #--32-bit
+            subprocess.call(['python', 'setup.py', 'build', '--plat-name=win', 'bdist_wininst'])    
+            #--64-bit
+            subprocess.call(['python', 'setup.py', 'build', '--plat-name=win-amd64', 'bdist_wininst'])
+    
+    #--now register the package with PyPI
+    if reg:
+        subprocess.call(['python', 'setup.py', 'register'])
 
-#--install the source
-subprocess.call(['python', 'setup.py', 'install'])
+if __name__ == "__main__":    
+    uver = False
+    utag = False
+    register = False
+    winbuild = False
+    major = False
+    minor = False
+    for arg in sys.argv:
+        if arg.lower() == '--update' or arg.lower() == '-u':
+            uver = True
+            sys.stdout.write('  Will update version number.\n')    
+        elif arg.lower() == '--register' or arg.lower() == '-r':
+            register = True
+            sys.stdout.write('  Will register version with PyPI.\n')    
+        elif arg.lower() == '--tag' or arg.lower() == '-t':
+            utag = True
+            sys.stdout.write('  Will update git tag with version number.\n')    
+        elif arg.lower() == '--win' or arg.lower() == '-w':
+            winbuild = True
+            sys.stdout.write('  Will build windows installers if possible.\n')    
+        elif arg.lower() == '--major' or arg.lower() == '-ma':
+            major = True
+            sys.stdout.write('  Will update major version number by one.\n')    
+        elif arg.lower() == '--minor' or arg.lower() == '-mi':
+            minor = True
+            sys.stdout.write('  Will update minor version number by one.\n')    
 
-#--create the source distribution
-subprocess.call(['python', 'setup.py', 'sdist', '--format=zip'])
-
-#--create 32-bit and 64-bit windows installers
-if 'windows' in platform.system().lower():
-    #--32-bit
-    subprocess.call(['python', 'setup.py', 'build', '--plat-name=win', 'bdist_wininst'])    
-    #--64-bit
-    subprocess.call(['python', 'setup.py', 'build', '--plat-name=win-amd64', 'bdist_wininst'])
-
-#--now register the package with PyPI
-subprocess.call(['python', 'setup.py', 'register'])
+    build_distribution(uver, utag, register, winbuild, major, minor)
    
--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ if not sys.version_info[0] in [2, 3]:
     print('  Your version of Python: {}'.format(sys.version_info[0]))
     sys.exit(1) # return non-zero value for failure
 
-reqs = [line.strip() for line in open('requirements.txt')]
-
 long_description = ''
  
 try:
@@ -33,9 +31,7 @@ setup(name=__name__,
       url='https://github.com/modflowpy/flopy/',
       license='New BSD',
       platforms='Windows, Mac OS-X',
-      install_requires=reqs,
+      install_requires=['numpy>=1.9', 'matplotlib>=1.4'],
       packages=['flopy', 'flopy.modflow', 'flopy.modpath', 'flopy.mt3d', 'flopy.seawat', 'flopy.utils', 'flopy.plot'],
-      package_data={'': ['requirements.txt']},
-      include_package_data=True,
       # use this version ID if .svn data cannot be found
       version=__version__ )
