03c7d0cb35dca27752d455d6276d87b8888ced41
Christian D Langevin <langevin@usgs.gov>
2015-10-22 09:53:39 -0400

    Added t008_test.py that runs through and loads all of the MF2005 example problems.

---
+++ py.test/t008_test.py
@@ -0,0 +1,15 @@
+"""
+Try to load all of the MODFLOW examples in ../examples/data/mf2005_test.
+These are the examples that are distributed with MODFLOW-2005.
+"""
+
+import os
+import flopy
+
+def test_modflow_load():
+    pth = os.path.join('..', 'examples', 'data', 'mf2005_test')
+    namfiles = [namfile for namfile in os.listdir(pth) if namfile.endswith('.nam')]
+    for namfile in namfiles:
+        m = flopy.modflow.Modflow.load(namfile, model_ws=pth, verbose=True)
+        assert m, 'Could not load namefile {}'.format(namfile)
+    return
