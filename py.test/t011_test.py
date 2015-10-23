a18f4e9fbce5ec7d5f4ae19363ddce75ec4c7964
Joseph D Hughes <jdhughes@usgs.gov>
2015-10-23 15:36:05 -0400

    Added function to extract extends from shapeless. Also added functionality to plot an array of data on a patch collection created from a shapefile. This has been used to plot simulated head results from an unstructured model (biscayne MODFLOW-USG model).

--- py.test/t011_test.py
+++ py.test/t011_test.py
@@ -9,10 +9,20 @@ import flopy
 
 
 def test_mflist():
-    model_ws = os.path.join("..", "examples", "data", "freyberg")
-    nam = "freyberg"
-    list_file = os.path.join(model_ws,nam+".lst")
+    model_ws = os.path.join('..', 'examples', 'data', 'freyberg')
+    nam = 'freyberg'
+    ml = flopy.modflow.Modflow.load(nam, model_ws=model_ws, 
+                                    exe_name='mf2005', version='mf2005')
+    new_ws = 'temp'
+    ml.change_model_ws(new_ws)
+    ml.write_input()
+    ml.run_model(silent=True)
+    
+    list_file = os.path.join(new_ws,'{}.list'.format(nam))
     assert os.path.exists(list_file)
     mflist = flopy.utils.MfListBudget(list_file)
     df_in,df_out = mflist.get_dataframes(start_datetime=None)
     return
+
+if __name__ == '__main__':
+    test_mflist()
