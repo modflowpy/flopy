1e33bce9c68d5e2247556be0ee10a3d89af5b1de
Joseph D Hughes <jdhughes@usgs.gov>
2015-10-28 17:41:57 -0400

    Added githds to repo so travis does not fail for test.

--- py.test/t006_test.py
+++ py.test/t006_test.py
@@ -1,19 +1,22 @@
 # Test reference class
 import matplotlib
+
 matplotlib.use('Agg')
+
 
 def test_binaryfile_reference():
     import os
     import matplotlib.pyplot as plt
     import flopy
-    
-    h = flopy.utils.HeadFile(os.path.join("..","examples","data","freyberg","freyberg.hds"))
+
+    h = flopy.utils.HeadFile(os.path.join('..', 'examples', 'data', 'freyberg', 'freyberg.githds'))
     h.sr.xul = 1000.0
     h.sr.yul = 200.0
     h.sr.rotation = 15.0
     h.plot()
-    plt.close("all")
+    plt.close('all')
     return
+
 
 def test_mflist_reference():
     import os
@@ -23,37 +26,37 @@ def test_mflist_reference():
     import flopy
     import flopy.modflow as fmf
 
-    #model_ws = os.path.join('..', 'data', 'freyberg')
-    #ml = fmf.Modflow.load('freyberg.nam', model_ws=model_ws)
+    # model_ws = os.path.join('..', 'data', 'freyberg')
+    # ml = fmf.Modflow.load('freyberg.nam', model_ws=model_ws)
     # make the model
     ml = fmf.Modflow()
-    perlen = np.arange(1,20,1)
+    perlen = np.arange(1, 20, 1)
     nstp = np.flipud(perlen) + 3
     tsmult = 1.2
     nlay = 10
-    nrow,ncol = 50,40
-    botm = np.arange(0,-100,-10)
-    hk = np.random.random((nrow,ncol))
+    nrow, ncol = 50, 40
+    botm = np.arange(0, -100, -10)
+    hk = np.random.random((nrow, ncol))
     dis = fmf.ModflowDis(ml, delr=100.0, delc=100.0,
                          nrow=nrow, ncol=ncol, nlay=nlay,
                          nper=perlen.shape[0], perlen=perlen,
                          nstp=nstp, tsmult=tsmult,
                          top=10, botm=botm, steady=False, rotation=45)
-    lpf = fmf.ModflowLpf(ml,hk=hk,vka=10.0,laytyp=1)
+    lpf = fmf.ModflowLpf(ml, hk=hk, vka=10.0, laytyp=1)
     pcg = fmf.ModflowPcg(ml)
-    oc =  fmf.ModflowOc(ml)
-    ibound = np.ones((nrow,ncol))
-    ibound[:,0] = -1
-    ibound[25:30,30:39] = 0
+    oc = fmf.ModflowOc(ml)
+    ibound = np.ones((nrow, ncol))
+    ibound[:, 0] = -1
+    ibound[25:30, 30:39] = 0
     bas = fmf.ModflowBas(ml, strt=5.0, ibound=ibound)
-    rch = fmf.ModflowRch(ml, rech={0:0.00001,5:0.0001,6:0.0})
+    rch = fmf.ModflowRch(ml, rech={0: 0.00001, 5: 0.0001, 6: 0.0})
     wel_dict = {}
-    wel_data = [[9,25,20,-200],[0,0,0,-400],[5,20,32,500]]
+    wel_data = [[9, 25, 20, -200], [0, 0, 0, -400], [5, 20, 32, 500]]
     wel_dict[0] = wel_data
-    wel_data2 = [[45,20,200],[9,49,39,400],[5,20,32,500]]
+    wel_data2 = [[45, 20, 200], [9, 49, 39, 400], [5, 20, 32, 500]]
     wel_dict[10] = wel_data2
-    wel = fmf.ModflowWel(ml, stress_period_data={0:wel_data})
-    ghb_dict = {0:[1,10,10,400,300]}
+    wel = fmf.ModflowWel(ml, stress_period_data={0: wel_data})
+    ghb_dict = {0: [1, 10, 10, 400, 300]}
     ghb = fmf.ModflowGhb(ml, stress_period_data=ghb_dict)
 
     test = os.path.join('temp', 'test3.shp')
