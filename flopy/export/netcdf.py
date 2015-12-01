8932ceafb33ade083573de2722ecc3b914c85933
Joseph D Hughes <jdhughes@usgs.gov>
2015-12-01 17:07:21 -0500

    Remove pandas dependency in netcdf.py - using python-dateutil to create iso datestring from passed string. Some pep8 formatting.

--- flopy/export/netcdf.py
+++ flopy/export/netcdf.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 # globals
 FILLVALUE = -99999.9
-ITMUNI = {0:"undefined",1:"seconds",2:"minutes",3:"hours",4:"days",5:"years"}
-LENUNI = {0:"undefined",1:"feet",2:"meters",3:"centimeters"}
-PRECISION_STRS = ["f4","f8","i4"]
+ITMUNI = {0: "undefined", 1: "seconds", 2: "minutes", 3: "hours", 4: "days", 5: "years"}
+LENUNI = {0: "undefined", 1: "feet", 2: "meters", 3: "centimeters"}
+PRECISION_STRS = ["f4", "f8", "i4"]
 
 
 class Logger(object):
@@ -27,21 +27,21 @@ class Logger(object):
             the item is popped from the keys
 
     """
-    def __init__(self,filename, echo=False):
+
+    def __init__(self, filename, echo=False):
         self.items = {}
         self.echo = bool(echo)
         if filename == True:
             self.echo = True
             self.filename = None
         elif filename:
-            self.f = open(filename, 'w', 0) #unbuffered
+            self.f = open(filename, 'w', 0)  # unbuffered
             self.t = datetime.now()
             self.log("opening " + str(filename) + " for logging")
         else:
             self.filename = None
 
-
-    def log(self,phrase):
+    def log(self, phrase):
         """log something that happened
         Parameters:
         ----------
@@ -53,19 +53,19 @@ class Logger(object):
             s = str(t) + ' finished: ' + str(phrase) + ", took: " + \
                 str(t - self.items[phrase]) + '\n'
             if self.echo:
-                print(s,)
+                print(s, )
             if self.filename:
                 self.f.write(s)
             self.items.pop(phrase)
         else:
             s = str(t) + ' starting: ' + str(phrase) + '\n'
             if self.echo:
-                print(s,)
+                print(s, )
             if self.filename:
                 self.f.write(s)
             self.items[phrase] = copy.deepcopy(t)
 
-    def warn(self,message):
+    def warn(self, message):
         """write a warning to the log file
         Parameters:
         ----------
@@ -74,7 +74,7 @@ class Logger(object):
         """
         s = str(datetime.now()) + " WARNING: " + message + '\n'
         if self.echo:
-            print(s,)
+            print(s, )
         if self.filename:
             self.f.write(s)
 
@@ -107,7 +107,7 @@ class NetCdf(object):
 
     """
 
-    def __init__(self,output_filename,model,time_values=None,verbose=None):
+    def __init__(self, output_filename, model, time_values=None, verbose=None):
 
         assert output_filename.lower().endswith(".nc")
         if verbose is None:
@@ -115,20 +115,22 @@ class NetCdf(object):
         self.logger = Logger(verbose)
         self.log = self.logger.log
         if os.path.exists(output_filename):
-            self.logger.warn("removing existing nc file: "+output_filename)
+            self.logger.warn("removing existing nc file: " + output_filename)
             os.remove(output_filename)
         self.output_filename = output_filename
 
         assert model.dis != None
         self.model = model
-        self.shape = (self.model.nlay,self.model.nrow,self.model.ncol)
+        self.shape = (self.model.nlay, self.model.nrow, self.model.ncol)
 
-        import pandas as pd
-        self.start_datetime = pd.to_datetime(self.model.dis.start_datetime).\
-                                  isoformat().split('.')[0].split('+')[0] + "Z"
+        # import pandas as pd
+        # self.start_datetime = pd.to_datetime(self.model.dis.start_datetime).\
+        #                          isoformat().split('.')[0].split('+')[0] + "Z"
+        import dateutil.parser
+        self.start_datetime = dateutil.parser.parse(self.model.dis.start_datetime).strftime("%Y-%m-%dT%H:%M:%SZ")
 
         self.grid_units = LENUNI[self.model.dis.lenuni]
-        assert self.grid_units in ["feet","meters"],\
+        assert self.grid_units in ["feet", "meters"], \
             "unsupported length units: " + self.grid_units
 
         self.time_units = ITMUNI[self.model.dis.itmuni]
@@ -144,17 +146,16 @@ class NetCdf(object):
             self.initialize_file(time_values=time_values)
             self.log("time_values != None, initializing file")
 
-
     def write(self):
         """ write the nc object to disk"""
         self.log("writing nc file")
-        assert self.nc is not None,"netcdf.write() error: nc file not initialized"
+        assert self.nc is not None, "netcdf.write() error: nc file not initialized"
 
         # write any new attributes that have been set since initializing the file
-        for k,v in self.global_attributes.items():
+        for k, v in self.global_attributes.items():
             try:
                 if self.nc.attributes.get(k) is not None:
-                    self.nc.setncattr(k,v)
+                    self.nc.setncattr(k, v)
             except Exception as e:
                 self.logger.warn('error setting global attribute {0}'.format(k))
 
@@ -162,12 +163,11 @@ class NetCdf(object):
         self.nc.close()
         self.log("writing nc file")
 
-
     def _initialize_attributes(self):
         """ private method to initial the attributes
             of the NetCdf instance
         """
-        assert "nc" not in self.__dict__.keys(),\
+        assert "nc" not in self.__dict__.keys(), \
             "NetCdf._initialize_attributes() error: nc attribute already set"
 
         self.nc_epsg_str = 'epsg:4326'
@@ -196,8 +196,6 @@ class NetCdf(object):
         self.origin_x = None
         self.origin_y = None
 
-
-
     def initialize_geometry(self):
         """ initialize the geometric information
             needed for the netcdf file
@@ -206,7 +204,6 @@ class NetCdf(object):
             from pyproj import Proj, transform
         except Exception as e:
             raise Exception("NetCdf error importing pyproj module:\n" + str(e))
-
 
         proj4_str = self.model.dis.sr.proj4_str
         if "epsg" in proj4_str.lower() and "init" not in proj4_str.lower():
@@ -219,9 +216,8 @@ class NetCdf(object):
             raise Exception("error building grid crs:\n{0}".format(str(e)))
         self.log("building grid crs using proj4 string: {0}".format(proj4_str))
 
-        #self.zs = -1.0 * self.model.dis.zcentroids[:,:,::-1]
+        # self.zs = -1.0 * self.model.dis.zcentroids[:,:,::-1]
         self.zs = -1.0 * self.model.dis.zcentroids
-
 
         ys = self.model.dis.sr.ycentergrid
         xs = self.model.dis.sr.xcentergrid
@@ -234,7 +230,7 @@ class NetCdf(object):
 
         # Transform to a known CRS
         nc_crs = Proj(init=self.nc_epsg_str)
-        self.log("projecting grid cell center arrays " +\
+        self.log("projecting grid cell center arrays " + \
                  "from {0} to {1}".format(str(self.grid_crs),
                                           str(nc_crs)))
         try:
@@ -243,16 +239,16 @@ class NetCdf(object):
             self.log("error projecting:\n{0}".format(str(e)))
             raise Exception("error projecting:\n{0}".format(str(e)))
 
-        self.log("projecting grid cell center arrays " +\
+        self.log("projecting grid cell center arrays " + \
                  "from {0} to {1}".format(str(self.grid_crs),
                                           str(nc_crs)))
 
         base_x = self.model.dis.sr.xgrid[0, 0]
         base_y = self.model.dis.sr.ygrid[0, 0]
-        self.origin_x,self.origin_y = transform(self.grid_crs,nc_crs,base_x,base_y)
+        self.origin_x, self.origin_y = transform(self.grid_crs, nc_crs, base_x, base_y)
         pass
 
-    def initialize_file(self,time_values=None):
+    def initialize_file(self, time_values=None):
         """ initialize the netcdf instance, including global attributes,
             dimensions, and grid information
 
@@ -283,13 +279,11 @@ class NetCdf(object):
         except Exception as e:
             raise Exception("error creating netcdf dataset:\n{0}".format(str(e)))
 
-
-
         # write some attributes
         self.log("setting standard attributes")
         self.nc.setncattr("Conventions", "CF-1.6")
         self.nc.setncattr("date_created", datetime.utcnow().strftime("%Y-%m-%dT%H:%M:00Z"))
-        self.nc.setncattr("geospatial_vertical_positive",   "up")
+        self.nc.setncattr("geospatial_vertical_positive", "up")
         min_vertical = np.min(self.zs)
         max_vertical = np.max(self.zs)
         self.nc.setncattr("geospatial_vertical_min", min_vertical)
@@ -328,75 +322,71 @@ class NetCdf(object):
         crs.inverse_flattening = self.nc_inverse_flat
         self.log("setting CRS info")
 
-
-
-        attribs = {"units":"{0} since {1}".format(self.time_units, self.start_datetime),
+        attribs = {"units": "{0} since {1}".format(self.time_units, self.start_datetime),
                    "standard_name": "time", "long_name": "time", "calendar": "gregorian",
-                   "_CoordinateAxisType":"Time"}
-        time = self.create_variable("time",attribs,precision_str="f8",dimensions=("time",))
+                   "_CoordinateAxisType": "Time"}
+        time = self.create_variable("time", attribs, precision_str="f8", dimensions=("time",))
         time[:] = np.asarray(time_values)
 
         # Elevation
-        attribs = {"units":"meters","standard_name":"elevation",
-                   "long_name":"elevation","axis":"Z",
-                   "valid_min":min_vertical,"valid_max":max_vertical,
-                   "positive":"down"}
-        elev = self.create_variable("elevation",attribs,precision_str="f8",dimensions=("layer","y","x"))
+        attribs = {"units": "meters", "standard_name": "elevation",
+                   "long_name": "elevation", "axis": "Z",
+                   "valid_min": min_vertical, "valid_max": max_vertical,
+                   "positive": "down"}
+        elev = self.create_variable("elevation", attribs, precision_str="f8", dimensions=("layer", "y", "x"))
         elev[:] = self.zs
 
-
         # Longitude
-        attribs = {"units":"degrees_east","standard_name":"longitude",
-                   "long_name":"longitude","axis":"X",
-                   "_CoordinateAxisType":"Lon"}
-        lon = self.create_variable("longitude",attribs,precision_str="f8",dimensions=("y","x"))
+        attribs = {"units": "degrees_east", "standard_name": "longitude",
+                   "long_name": "longitude", "axis": "X",
+                   "_CoordinateAxisType": "Lon"}
+        lon = self.create_variable("longitude", attribs, precision_str="f8", dimensions=("y", "x"))
         lon[:] = self.xs
         self.log("creating longitude var")
 
         # Latitude
         self.log("creating latitude var")
-        attribs = {"units":"degrees_north","standard_name":"latitude",
-                   "long_name":"latitude","axis":"Y",
-                   "_CoordinateAxisType":"Lat"}
-        lat = self.create_variable("latitude",attribs,precision_str="f8",dimensions=("y","x"))
+        attribs = {"units": "degrees_north", "standard_name": "latitude",
+                   "long_name": "latitude", "axis": "Y",
+                   "_CoordinateAxisType": "Lat"}
+        lat = self.create_variable("latitude", attribs, precision_str="f8", dimensions=("y", "x"))
         lat[:] = self.ys
-
 
         # layer
         self.log("creating layer var")
-        attribs = {"units":"","standard_name":"layer","long_name":"layer",
-                   "positive":"down","axis":"Z"}
-        lay = self.create_variable("layer",attribs,dimensions=("layer",))
+        attribs = {"units": "", "standard_name": "layer", "long_name": "layer",
+                   "positive": "down", "axis": "Z"}
+        lay = self.create_variable("layer", attribs, dimensions=("layer",))
         lay[:] = np.arange(0, self.shape[0])
         self.log("creating layer var")
 
         # delc
-        attribs = {"units":"meters","long_names":"row spacing",
-                   "origin_x":self.model.dis.sr.xul,
-                   "origin_y":self.model.dis.sr.yul,
-                   "origin_crs":self.nc_epsg_str}
+        attribs = {"units": "meters", "long_names": "row spacing",
+                   "origin_x": self.model.dis.sr.xul,
+                   "origin_y": self.model.dis.sr.yul,
+                   "origin_crs": self.nc_epsg_str}
         delc = self.create_variable('delc', attribs, dimensions=('y',))
         if self.grid_units.lower().startswith('f'):
             delc[:] = self.model.dis.delc.array[::-1] * 0.3048
         else:
             delc[:] = self.model.dis.delc.array[::-1]
         if self.model.dis.sr.rotation != 0:
-            delc.comments = "This is the row spacing that applied to the UNROTATED grid. " +\
-                            "This grid HAS been rotated before being saved to NetCDF. " +\
+            delc.comments = "This is the row spacing that applied to the UNROTATED grid. " + \
+                            "This grid HAS been rotated before being saved to NetCDF. " + \
                             "To compute the unrotated grid, use the origin point and this array."
         # delr
-        attribs = {"units":"meters","long_names":"col spacing",
-                   "origin_x":self.model.dis.sr.xul,
-                   "origin_y":self.model.dis.sr.yul,
-                   "origin_crs":self.nc_epsg_str}
+        attribs = {"units": "meters", "long_names": "col spacing",
+                   "origin_x": self.model.dis.sr.xul,
+                   "origin_y": self.model.dis.sr.yul,
+                   "origin_crs": self.nc_epsg_str}
         delr = self.create_variable('delr', attribs, dimensions=('x',))
         if self.grid_units.lower().startswith('f'):
             delr[:] = self.model.dis.delr.array[::-1] * 0.3048
         else:
             delr[:] = self.model.dis.delr.array[::-1]
         if self.model.dis.sr.rotation != 0:
-            delr.comments = "This is the col spacing that applied to the UNROTATED grid. " +\
-                            "This grid HAS been rotated before being saved to NetCDF. " +\
+            delr.comments = "This is the col spacing that applied to the UNROTATED grid. " + \
+                            "This grid HAS been rotated before being saved to NetCDF. " + \
                             "To compute the unrotated grid, use the origin point and this array."
 
         # Workaround for CF/CDM.
@@ -436,9 +426,9 @@ class NetCdf(object):
         """
 
         self.log("creating variable: " + str(name))
-        assert precision_str in PRECISION_STRS,\
-            "netcdf.create_variable() error: precision string {0} not in {1}".\
-                format(precision_str,PRECISION_STRS)
+        assert precision_str in PRECISION_STRS, \
+            "netcdf.create_variable() error: precision string {0} not in {1}". \
+                format(precision_str, PRECISION_STRS)
 
         if self.nc is None:
             self.initialize_file()
@@ -447,17 +437,17 @@ class NetCdf(object):
         # build up the chuck sizes
         chunks = []
         for dimension in dimensions:
-            assert self.nc.dimensions.get(dimension) is not None,\
+            assert self.nc.dimensions.get(dimension) is not None, \
                 "netcdf.create_variable() dimension not found:" + dimension
             chunk = self.chunks[dimension]
-            assert chunk is not None,\
-                "netcdf.create_variable() chunk size of {0} is None in self.chunks".\
-                format(dimension)
+            assert chunk is not None, \
+                "netcdf.create_variable() chunk size of {0} is None in self.chunks". \
+                    format(dimension)
             chunks.append(chunk)
 
         # Normalize variable name
         name = name.replace('.', '_').replace(' ', '_').replace('-', '_')
-        assert self.nc.variables.get(name) is None,\
+        assert self.nc.variables.get(name) is None, \
             "netcdf.create_variable error: variable already exists:" + name
 
         var = self.nc.createVariable(name, precision_str, dimensions,
@@ -468,11 +458,7 @@ class NetCdf(object):
             try:
                 var.setncattr(k, v)
             except:
-                self.logger.warn("error setting attribute" +\
-                                 "{0} for variable {1}".format(k,name))
+                self.logger.warn("error setting attribute" + \
+                                 "{0} for variable {1}".format(k, name))
         self.log("creating variable: " + str(name))
         return var
-
-
-
-
