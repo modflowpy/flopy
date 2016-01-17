6b4ff78f3a68f7ab43f558359ce531a167bd0f6f
Christian D Langevin <langevin@usgs.gov>
2016-01-17 17:23:56 -0500

    Making t023_test a little tougher.

--- autotest/t023_test.py
+++ autotest/t023_test.py
@@ -1,5 +1,6 @@
 # Test multi-species options in mt3d
 import os
+import numpy as np
 import flopy
 
 testpth = os.path.join('.', 'temp')
@@ -19,35 +20,33 @@ def test_mt3d_multispecies():
     evt = flopy.modflow.ModflowEvt(mf)
     mf.write_input()
 
-    # mt3d model
+    # Create a 5-component mt3d model and write the files
     ncomp = 5
     mt = flopy.mt3d.Mt3dms(modelname=modelname, modflowmodel=mf,
-                           model_ws=testpth)
-    btn = flopy.mt3d.Mt3dBtn(mt, ncomp=ncomp, sconc=1., sconc2=2., sconc3=3.,
-                             sconc5=5.)
-    ssm = flopy.mt3d.Mt3dSsm(mt, crch=1., crch2=2., crch3={2:3.}, crch5=5.,
-                             cevt=1., cevt2=2., cevt3={2:3.}, cevt5=5.)
-
+                           model_ws=testpth, verbose=True)
+    sconc3 = np.random.random((nrow, ncol))
+    btn = flopy.mt3d.Mt3dBtn(mt, ncomp=ncomp, sconc=1., sconc2=2.,
+                             sconc3=sconc3, sconc5=5.)
+    crch32 = np.random.random((nrow, ncol))
+    cevt33 = np.random.random((nrow, ncol))
+    ssm = flopy.mt3d.Mt3dSsm(mt, crch=1., crch2=2., crch3={2:crch32}, crch5=5.,
+                             cevt=1., cevt2=2., cevt3={3:cevt33}, cevt5=5.)
     crch2 = ssm.crch[1].array
     assert(crch2.max() == 2.)
     cevt2 = ssm.cevt[1].array
     assert(cevt2.max() == 2.)
-
     mt.write_input()
 
-    # Try to load ssm
+    # Create a second MODFLOW model
     modelname2 = 'multispecies2'
     mf2 = flopy.modflow.Modflow(modelname=modelname2, model_ws=testpth)
     dis2 = flopy.modflow.ModflowDis(mf2, nlay=nlay, nrow=nrow, ncol=ncol,
                                     nper=nper)
 
-    mt2 = flopy.mt3d.Mt3dms(modelname=modelname2, modflowmodel=mf2,
-                            model_ws=testpth)
-    mt2.verbose = True
-    fname = os.path.join(testpth, modelname + '.btn')
-    btn2 = flopy.mt3d.Mt3dBtn.load(fname, mt2)
-    fname = os.path.join(testpth, modelname + '.ssm')
-    ssm2 = flopy.mt3d.Mt3dSsm.load(fname, mt2)
+    # Load the MT3D model into mt2 and then write it out
+    fname = modelname + '.nam'
+    mt2 = flopy.mt3d.Mt3dms.load(fname, model_ws=testpth, verbose=True)
+    mt2.name = modelname2
     mt2.write_input()
 
     return
