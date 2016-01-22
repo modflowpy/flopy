3d91a7027dcdf1ff57b163e5e95b45918cdcf810
jwhite <jtwhite1000@gmail.com>
2016-01-22 14:11:51 -0600

    added helper function in flopy_io to convert any CellBudgetFile entry (e.g. "DRAIN","RIVER","MNW") to an ModflowWel instance - a cheap and easy work around for not having MNW support.

--- flopy/utils/binaryfile.py
+++ flopy/utils/binaryfile.py
@@ -423,6 +423,7 @@ class CellBudgetFile(object):
         self.nrow = 0
         self.ncol = 0
         self.nlay = 0
+        self.nper = 0
         self.times = []
         self.kstpkper = []
         self.recordarray = []
@@ -465,8 +466,8 @@ class CellBudgetFile(object):
         self._build_index()
         
         # allocate the value array
-        self.value = np.empty((self.nlay, self.nrow, self.ncol),
-                              dtype=self.realtype)
+        #self.value = np.empty((self.nlay, self.nrow, self.ncol),
+        #                      dtype=self.realtype)
         return
    
     def _build_index(self):
@@ -475,6 +476,9 @@ class CellBudgetFile(object):
         to the position in the binary file.
         """
         header = self._get_header()
+        self.nrow = header["nrow"]
+        self.ncol = header["ncol"]
+        self.nlay = np.abs(header["nlay"])
         self.file.seek(0, 2)
         self.totalbytes = self.file.tell()
         self.file.seek(0, 0)
@@ -517,7 +521,7 @@ class CellBudgetFile(object):
         # convert to numpy arrays
         self.recordarray = np.array(self.recordarray, dtype=self.header_dtype)
         self.iposarray = np.array(self.iposarray, dtype=np.int64)
-
+        self.nper = self.recordarray["kper"].max()
         return
 
     def _skip_record(self, header):
