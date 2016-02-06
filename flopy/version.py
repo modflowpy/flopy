cc5fcf858b6f746a2b7bae9894ea9a434bc20620
Joseph D Hughes <jdhughes@usgs.gov>
2016-02-06 18:14:53 -0500

    Final updates prior to release.

--- flopy/version.py
+++ flopy/version.py
@@ -3,7 +3,7 @@
 major = 3
 minor = 2
 micro = 4
-commit = 1261
+commit = 1265
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, commit)
