65787da2a3d4d9aa1b6e1c3c4c6f50536f65a80e
Christian D Langevin <langevin@usgs.gov>
2016-06-12 15:54:40 -0400

    More on reaction package loading.

--- flopy/mt3d/mtrct.py
+++ flopy/mt3d/mtrct.py
@@ -5,9 +5,9 @@ from ..utils import Util3d
 
 
 class Mt3dRct(Package):
-    '''
+    """
     Chemical reaction package class
-    '''
+    """
     unitnumber = 36
 
     def __init__(self, model, isothm=0, ireact=0, igetsc=1, rhob=1.8e3,
@@ -130,217 +130,216 @@ class Mt3dRct(Package):
         return
 
 
-@staticmethod
-def load(f, model, nlay=None, nrow=None, ncol=None, nper=None,
-         ncomp=None, ext_unit_dict=None):
-    """
-    Load an existing package.
+    @staticmethod
+    def load(f, model, nlay=None, nrow=None, ncol=None, nper=None,
+             ncomp=None, ext_unit_dict=None):
+        """
+        Load an existing package.
 
-    Parameters
-    ----------
-    f : filename or file handle
-        File to load.
-    model : model object
-        The model object (of type :class:`flopy.mt3d.mt.Mt3dms`) to
-        which this package will be added.
-    ext_unit_dict : dictionary, optional
-        If the arrays in the file are specified using EXTERNAL,
-        or older style array control records, then `f` should be a file
-        handle.  In this case ext_unit_dict is required, which can be
-        constructed using the function
-        :class:`flopy.utils.mfreadnam.parsenamefile`.
+        Parameters
+        ----------
+        f : filename or file handle
+            File to load.
+        model : model object
+            The model object (of type :class:`flopy.mt3d.mt.Mt3dms`) to
+            which this package will be added.
+        ext_unit_dict : dictionary, optional
+            If the arrays in the file are specified using EXTERNAL,
+            or older style array control records, then `f` should be a file
+            handle.  In this case ext_unit_dict is required, which can be
+            constructed using the function
+            :class:`flopy.utils.mfreadnam.parsenamefile`.
 
-    Returns
-    -------
-    rct :  Mt3dRct object
-        Mt3dRct object.
+        Returns
+        -------
+        rct :  Mt3dRct object
+            Mt3dRct object.
 
-    Examples
-    --------
+        Examples
+        --------
 
-    >>> import flopy
-    >>> mt = flopy.mt3d.Mt3dms()
-    >>> rct = flopy.mt3d.Mt3dRct.load('test.rct', mt)
+        >>> import flopy
+        >>> mt = flopy.mt3d.Mt3dms()
+        >>> rct = flopy.mt3d.Mt3dRct.load('test.rct', mt)
 
-    """
+        """
 
-    if model.verbose:
-        sys.stdout.write('loading rct package file...\n')
-
-    # Open file, if necessary
-    if not hasattr(f, 'read'):
-        filename = f
-        f = open(filename, 'r')
-
-    # Set dimensions if necessary
-    if nlay is None:
-        nlay = model.nlay
-    if nrow is None:
-        nrow = model.nrow
-    if ncol is None:
-        ncol = model.ncol
-    if nper is None:
-        nper = model.nper
-    if ncomp is None:
-        ncomp = model.ncomp
-
-    # Setup kwargs to store multispecies arguments
-    kwargs = {}
-
-    # Item E1
-    line = f.readline()
-    if model.verbose:
-        print('   loading ISOTHM, IREACT, IRCTOP, IGETSC...')
-    line = f.readline()
-    isothm = int(line[0:10])
-    ireact = int(line[11:20])
-    try:
-        irctop = int(line[21:30])
-    except:
-        irctop = 0
-    try:
-        igetsc = int(line[31:40])
-    except:
-        igetsc = 0
-    if model.verbose:
-        print('   ISOTHM {}'.format(isothm))
-        print('   IREACT {}'.format(ireact))
-        print('   IRCTOP {}'.format(irctop))
-        print('   IGETSC {}'.format(igetsc))
-
-    # Item E2A: RHOB
-    rhob = None
-    if model.verbose:
-        print('   loading RHOB...')
-    if isothm in [1, 2, 3, 4, 6]:
-        rhob = Util3d.load(f, model, (nlay, nrow, ncol), np.float32, 'rhob',
-                             ext_unit_dict, array_format="mt3d")
         if model.verbose:
-            print('   RHOB {}'.format(rhob))
+            sys.stdout.write('loading rct package file...\n')
 
-    # Item E2A: PRSITY2
-    prsity2 = None
-    if model.verbose:
-        print('   loading PRSITY2...')
-    if isothm in [5, 6]:
-        prsity2 = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
-                           'prsity2',
-                           ext_unit_dict, array_format="mt3d")
+        # Open file, if necessary
+        if not hasattr(f, 'read'):
+            filename = f
+            f = open(filename, 'r')
+
+        # Set dimensions if necessary
+        if nlay is None:
+            nlay = model.nlay
+        if nrow is None:
+            nrow = model.nrow
+        if ncol is None:
+            ncol = model.ncol
+        if nper is None:
+            nper = model.nper
+        if ncomp is None:
+            ncomp = model.ncomp
+
+        # Setup kwargs to store multispecies arguments
+        kwargs = {}
+
+        # Item E1
+        line = f.readline()
         if model.verbose:
-            print('   PRSITY2 {}'.format(prsity2))
-
-    # Item E2C: SRCONC
-    srconc = None
-    if model.verbose:
-        print('   loading SRCONC...')
-    if igetsc > 0:
-        srconc = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
-                             'srconc1', ext_unit_dict, array_format="mt3d")
+            print('   loading ISOTHM, IREACT, IRCTOP, IGETSC...')
+        isothm = int(line[0:10])
+        ireact = int(line[11:20])
+        try:
+            irctop = int(line[21:30])
+        except:
+            irctop = 0
+        try:
+            igetsc = int(line[31:40])
+        except:
+            igetsc = 0
         if model.verbose:
-            print('   SRCONC {}'.format(srconc))
-        if ncomp > 1:
-            for icomp in range(2, ncomp + 1):
-                name = "srconc" + str(icomp)
-                if model.verbose:
-                    print('   loading {}...'.format(name))
-                u3d = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
-                                   name, ext_unit_dict, array_format="mt3d")
-                kwargs[name] = u3d
-                if model.verbose:
-                    print('   SRCONC{} {}'.format(icomp, u3d))
+            print('   ISOTHM {}'.format(isothm))
+            print('   IREACT {}'.format(ireact))
+            print('   IRCTOP {}'.format(irctop))
+            print('   IGETSC {}'.format(igetsc))
 
-    # Item E3: SP1
-    sp1 = None
-    if model.verbose:
-        print('   loading SP1...')
-    if isothm > 0:
-        sp1 = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
-                             'sp11', ext_unit_dict, array_format="mt3d")
+        # Item E2A: RHOB
+        rhob = None
         if model.verbose:
-            print('   SP1 {}'.format(sp1))
-        if ncomp > 1:
-            for icomp in range(2, ncomp + 1):
-                name = "sp1" + str(icomp)
-                if model.verbose:
-                    print('   loading {}...'.format(name))
-                u3d = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
-                                  name, ext_unit_dict, array_format="mt3d")
-                kwargs[name] = u3d
-                if model.verbose:
-                    print('   SP1{} {}'.format(icomp, u3d))
+            print('   loading RHOB...')
+        if isothm in [1, 2, 3, 4, 6]:
+            rhob = Util3d.load(f, model, (nlay, nrow, ncol), np.float32, 'rhob',
+                                 ext_unit_dict, array_format="mt3d")
+            if model.verbose:
+                print('   RHOB {}'.format(rhob))
 
-    # Item E4: SP2
-    sp2 = None
-    if model.verbose:
-        print('   loading SP2...')
-    if isothm > 0:
-        sp2 = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
-                             'sp21', ext_unit_dict, array_format="mt3d")
+        # Item E2A: PRSITY2
+        prsity2 = None
         if model.verbose:
-            print('   SP2 {}'.format(sp2))
-        if ncomp > 1:
-            for icomp in range(2, ncomp + 1):
-                name = "sp2" + str(icomp)
-                if model.verbose:
-                    print('   loading {}...'.format(name))
-                u3d = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
-                                  name, ext_unit_dict, array_format="mt3d")
-                kwargs[name] = u3d
-                if model.verbose:
-                    print('   SP2{} {}'.format(icomp, u3d))
+            print('   loading PRSITY2...')
+        if isothm in [5, 6]:
+            prsity2 = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
+                               'prsity2',
+                               ext_unit_dict, array_format="mt3d")
+            if model.verbose:
+                print('   PRSITY2 {}'.format(prsity2))
 
-    # Item E5: RC1
-    rc1 = None
-    if model.verbose:
-        print('   loading RC1...')
-    if ireact > 0:
-        rc1 = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
-                          'rc11', ext_unit_dict,
-                          array_format="mt3d")
+        # Item E2C: SRCONC
+        srconc = None
         if model.verbose:
-            print('   RC1 {}'.format(rc1))
-        if ncomp > 1:
-            for icomp in range(2, ncomp + 1):
-                name = "rc1" + str(icomp)
-                if model.verbose:
-                    print('   loading {}...'.format(name))
-                u3d = Util3d.load(f, model, (nlay, nrow, ncol),
-                                  np.float32,
-                                  name, ext_unit_dict,
-                                  array_format="mt3d")
-                kwargs[name] = u3d
-                if model.verbose:
-                    print('   RC1{} {}'.format(icomp, u3d))
+            print('   loading SRCONC...')
+        if igetsc > 0:
+            srconc = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
+                                 'srconc1', ext_unit_dict, array_format="mt3d")
+            if model.verbose:
+                print('   SRCONC {}'.format(srconc))
+            if ncomp > 1:
+                for icomp in range(2, ncomp + 1):
+                    name = "srconc" + str(icomp)
+                    if model.verbose:
+                        print('   loading {}...'.format(name))
+                    u3d = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
+                                       name, ext_unit_dict, array_format="mt3d")
+                    kwargs[name] = u3d
+                    if model.verbose:
+                        print('   SRCONC{} {}'.format(icomp, u3d))
 
-    # Item E6: RC2
-    rc2 = None
-    if model.verbose:
-        print('   loading RC2...')
-    if ireact > 0:
-        rc2 = Util3d.load(f, model, (nlay, nrow, ncol),
-                          np.float32,
-                          'rc21', ext_unit_dict,
-                          array_format="mt3d")
+        # Item E3: SP1
+        sp1 = None
         if model.verbose:
-            print('   RC2 {}'.format(rc2))
-        if ncomp > 1:
-            for icomp in range(2, ncomp + 1):
-                name = "rc2" + str(icomp)
-                if model.verbose:
-                    print('   loading {}...'.format(name))
-                u3d = Util3d.load(f, model, (nlay, nrow, ncol),
-                                  np.float32,
-                                  name, ext_unit_dict,
-                                  array_format="mt3d")
-                kwargs[name] = u3d
-                if model.verbose:
-                    print('   RC2{} {}'.format(icomp, u3d))
+            print('   loading SP1...')
+        if isothm > 0:
+            sp1 = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
+                                 'sp11', ext_unit_dict, array_format="mt3d")
+            if model.verbose:
+                print('   SP1 {}'.format(sp1))
+            if ncomp > 1:
+                for icomp in range(2, ncomp + 1):
+                    name = "sp1" + str(icomp)
+                    if model.verbose:
+                        print('   loading {}...'.format(name))
+                    u3d = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
+                                      name, ext_unit_dict, array_format="mt3d")
+                    kwargs[name] = u3d
+                    if model.verbose:
+                        print('   SP1{} {}'.format(icomp, u3d))
 
-    # Close the file
-    f.close()
+        # Item E4: SP2
+        sp2 = None
+        if model.verbose:
+            print('   loading SP2...')
+        if isothm > 0:
+            sp2 = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
+                                 'sp21', ext_unit_dict, array_format="mt3d")
+            if model.verbose:
+                print('   SP2 {}'.format(sp2))
+            if ncomp > 1:
+                for icomp in range(2, ncomp + 1):
+                    name = "sp2" + str(icomp)
+                    if model.verbose:
+                        print('   loading {}...'.format(name))
+                    u3d = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
+                                      name, ext_unit_dict, array_format="mt3d")
+                    kwargs[name] = u3d
+                    if model.verbose:
+                        print('   SP2{} {}'.format(icomp, u3d))
 
-    # Construct and return rct package
-    rct = Mt3dRct(model)
-    return rct
+        # Item E5: RC1
+        rc1 = None
+        if model.verbose:
+            print('   loading RC1...')
+        if ireact > 0:
+            rc1 = Util3d.load(f, model, (nlay, nrow, ncol), np.float32,
+                              'rc11', ext_unit_dict,
+                              array_format="mt3d")
+            if model.verbose:
+                print('   RC1 {}'.format(rc1))
+            if ncomp > 1:
+                for icomp in range(2, ncomp + 1):
+                    name = "rc1" + str(icomp)
+                    if model.verbose:
+                        print('   loading {}...'.format(name))
+                    u3d = Util3d.load(f, model, (nlay, nrow, ncol),
+                                      np.float32,
+                                      name, ext_unit_dict,
+                                      array_format="mt3d")
+                    kwargs[name] = u3d
+                    if model.verbose:
+                        print('   RC1{} {}'.format(icomp, u3d))
+
+        # Item E6: RC2
+        rc2 = None
+        if model.verbose:
+            print('   loading RC2...')
+        if ireact > 0:
+            rc2 = Util3d.load(f, model, (nlay, nrow, ncol),
+                              np.float32,
+                              'rc21', ext_unit_dict,
+                              array_format="mt3d")
+            if model.verbose:
+                print('   RC2 {}'.format(rc2))
+            if ncomp > 1:
+                for icomp in range(2, ncomp + 1):
+                    name = "rc2" + str(icomp)
+                    if model.verbose:
+                        print('   loading {}...'.format(name))
+                    u3d = Util3d.load(f, model, (nlay, nrow, ncol),
+                                      np.float32,
+                                      name, ext_unit_dict,
+                                      array_format="mt3d")
+                    kwargs[name] = u3d
+                    if model.verbose:
+                        print('   RC2{} {}'.format(icomp, u3d))
+
+        # Close the file
+        f.close()
+
+        # Construct and return rct package
+        rct = Mt3dRct(model)
+        return rct
 
 
