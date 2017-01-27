e94e9f677e62e51ebd088eab99c977a68f9c83d3
Joseph D Hughes <jdhughes@usgs.gov>
2017-01-27 17:53:30 -0500

    Added observed of observed HOB data original and new.

--- autotest/t041_test.py
+++ autotest/t041_test.py
@@ -73,7 +73,7 @@ def test_obs_load_and_write():
         assert np.allclose(obs0[:, 0], obs1[:, 0], atol=1e-4), msg
 
         msg = 'new observed heads are not approximately equal'
-        assert np.allclose(obs0[:, 1], obs1[:, 1], atol=1e-6), msg
+        assert np.allclose(obs0[:, 1], obs1[:, 1], atol=1e-4), msg
 
 
 if __name__ == '__main__':
