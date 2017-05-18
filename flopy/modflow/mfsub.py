bug fix in sub: if a ds15 is passed, we need to use it instead of overwriting it - need to use  units from load()

flopy/modflow/mfsub.py
@@ -342,15 +342,16 @@ class ModflowSub(Package):
         if isuboc > 0:
             if ids15 is None:
                 ids15 = np.zeros(12, dtype=np.int)
+                iu = 0
+                for i in range(1, 12, 2):
+                    ids15[i] = item15_units[iu]
+                    iu += 1
+                self.ids15 = ids15
             else:
                 if isinstance(ids15, list):
                     ids15 = np.array(ids15)
-            # make sure the correct unit is specified
-            iu = 0
-            for i in range(1, 12, 2):
-                ids15[i] = item15_units[iu]
-                iu += 1
-            self.ids15 = ids15
+                self.ids15 = ids15
+
             if ids16 is None:
                 self.isuboc = 1
                 # save and print everything