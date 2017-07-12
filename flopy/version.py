ea30a0e9938b289572d037aedfc900dedb6f611a
Joseph D Hughes <jdhughes@usgs.gov>
2017-07-12 18:21:23 -0400

    Updates to travis requirements to use most recent versions of package where possible.

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...July 12, 2017 17:09:17
+# created on...July 12, 2017 18:21:23
 
 major = 3
 minor = 2
 micro = 6
-build = 91
-commit = 2163
+build = 92
+commit = 2164
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
