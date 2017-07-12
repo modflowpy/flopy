018c2641110f03541295d9a8a6a413773bf6a6bf
Joseph D Hughes <jdhughes@usgs.gov>
2017-07-12 18:25:10 -0400

    Missed a file.

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...July 12, 2017 18:21:23
+# created on...July 12, 2017 18:25:10
 
 major = 3
 minor = 2
 micro = 6
-build = 92
-commit = 2164
+build = 93
+commit = 2165
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
