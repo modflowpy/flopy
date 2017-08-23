57078e1bd683d5ddffc56824237e7bf62c025ded
Joseph D Hughes <jdhughes@usgs.gov>
2017-08-23 17:17:26 -0400

    Update autotest to build modflow, modflow-nwt, and modflow-usg if they don't exist.

--- autotest/t000_test.py
+++ autotest/t000_test.py
@@ -1,6 +1,17 @@
 # Remove the temp directory and then create a fresh one
 import os
+import sys
 import shutil
+import platform
+import flopy
+import pymake
+
+fc = 'gfortran'
+cc = 'gcc'
+double = False
+bindir = os.path.join(os.path.expanduser('~'), '.local', 'bin')
+print(bindir)
+
 
 def test_setup():
     tempdir = os.path.join('.', 'temp')
@@ -9,5 +20,122 @@ def test_setup():
     os.mkdir(tempdir)
     return
 
+
+def test_build_modflow():
+    starget = 'MODFLOW-2005'
+    exe_name = 'mf2005'
+    dirname = 'MF2005.1_12u'
+    url = "https://water.usgs.gov/ogw/modflow/MODFLOW-2005_v1.12.00/MF2005.1_12u.zip"
+
+    build_target(starget, exe_name, url, dirname)
+
+    return
+
+
+def test_build_mfnwt():
+    starget = 'MODFLOW-NWT'
+    exe_name = 'mfnwt'
+    dirname = 'MODFLOW-NWT_1.1.2'
+    url = "http://water.usgs.gov/ogw/modflow-nwt/{0}.zip".format(dirname)
+
+    build_target(starget, exe_name, url, dirname)
+
+    return
+
+
+def test_build_usg():
+    starget = 'MODFLOW-USG'
+    exe_name = 'mfusg'
+    dirname = 'mfusg.1_3'
+    url = 'http://water.usgs.gov/ogw/mfusg/{0}.zip'.format(dirname)
+
+    build_target(starget, exe_name, url, dirname)
+    return
+
+
+def set_compiler():
+    fct = fc
+    cct = cc
+    # parse command line arguments to see if user specified options
+    # relative to building the target
+    msg = ''
+    for idx, arg in enumerate(sys.argv):
+        if arg.lower() == '--ifort':
+            if len(msg) > 0:
+                msg += '\n'
+            msg += '{} - '.format(arg.lower()) + \
+                   '{} will be built with ifort.'.format(starget)
+            fct = 'ifort'
+        elif arg.lower() == '--cl':
+            if len(msg) > 0:
+                msg += '\n'
+            msg += '{} - '.format(arg.lower()) + \
+                   '{} will be built with cl.'.format(starget)
+            cct = 'cl'
+        elif arg.lower() == '--clang':
+            if len(msg) > 0:
+                msg += '\n'
+            msg += '{} - '.format(arg.lower()) + \
+                   '{} will be built with clang.'.format(starget)
+            cct = 'clang'
+    if len(msg) > 0:
+        print(msg)
+
+    return fct, cct
+
+
+def build_target(starget, exe_name, url, dirname):
+    print('Determining if {} needs to be built'.format(starget))
+    if platform.system().lower() == 'windows':
+        exe_name += '.exe'
+
+    exe_exists = flopy.which(exe_name)
+    if exe_exists is not None:
+        print('No need to build {} since it exists in the current path')
+        return
+
+    fct, cct = set_compiler()
+
+    # set up target
+    target = os.path.abspath(os.path.join(bindir, exe_name))
+
+    # get current directory
+    cpth = os.getcwd()
+
+    # create temporary path
+    dstpth = os.path.join('tempbin')
+    print('create...{}'.format(dstpth))
+    if not os.path.exists(dstpth):
+        os.makedirs(dstpth)
+    os.chdir(dstpth)
+
+    # Download the distribution
+    pymake.download_and_unzip(url)
+
+    # Set srcdir name
+    srcdir = os.path.join(dirname, 'src')
+
+    # compile code
+    print('compiling...{}'.format(os.path.relpath(target)))
+    pymake.main(srcdir, target, fct, cct, makeclean=True,
+                expedite=False, dryrun=False, double=double, debug=False)
+
+    msg = '{} does not exist.'.format(os.path.relpath(target))
+    assert os.path.isfile(target), msg
+
+    # change back to original path
+    os.chdir(cpth)
+
+    # Clean up downloaded directory
+    print('delete...{}'.format(dstpth))
+    if os.path.isdir(dstpth):
+        shutil.rmtree(dstpth)
+
+    return
+
+
 if __name__ == '__main__':
     test_setup()
+    test_build_modflow()
+    test_build_mfnwt()
+    test_build_usg()
--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...August 01, 2017 17:35:57
+# created on...August 23, 2017 17:17:26
 
 major = 3
 minor = 2
 micro = 6
-build = 99
-commit = 2171
+build = 124
+commit = 2196
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
