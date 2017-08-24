c29af27920718cf3e7c0caaa52526ba6749bc541
Joseph D Hughes <jdhughes@usgs.gov>
2017-08-24 14:38:30 -0400

    Not caching .local on Travis

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...August 24, 2017 10:26:36
+# created on...August 24, 2017 14:38:30
 
 major = 3
 minor = 2
 micro = 6
-build = 126
-commit = 2198
+build = 127
+commit = 2199
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
