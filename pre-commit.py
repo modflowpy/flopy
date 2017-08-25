7c9cfe18abe4c85fb4a551534e3ff0dfe3d8d189
Joseph D Hughes <jdhughes@usgs.gov>
2017-08-25 13:55:58 -0400

    Update precommit.py to automatically update version number and branch in README.md

--- pre-commit.py
+++ pre-commit.py
@@ -6,28 +6,32 @@ import os
 import sys
 import datetime
 
+files = ['version.py', 'README.md']
+
 pak = 'flopy'
+
+
 def get_version_str(v0, v1, v2, v3):
-    version_type = ('{}'.format(v0), 
-                    '{}'.format(v1), 
-                    '{}'.format(v2), 
+    version_type = ('{}'.format(v0),
+                    '{}'.format(v1),
+                    '{}'.format(v2),
                     '{}'.format(v3))
     version = '.'.join(version_type)
-    return version 
+    return version
 
-    
+
 def get_tag(v0, v1, v2):
-    tag_type = ('{}'.format(v0), 
-                '{}'.format(v1), 
+    tag_type = ('{}'.format(v0),
+                '{}'.format(v1),
                 '{}'.format(v2))
     tag = '.'.join(tag_type)
     return tag
-     
+
 
 def update_version():
     try:
-        pth = os.path.join(pak, 'version.py')
-        
+        pth = os.path.join(pak, files[0])
+
         vmajor = 0
         vminor = 0
         vmicro = 0
@@ -43,9 +47,9 @@ def update_version():
                 vmicro = int(t[2])
             elif 'build =' in line:
                 vbuild = int(t[2])
-        
+
         v0 = get_version_str(vmajor, vminor, vmicro, vbuild)
-        
+
         # get latest build number
         tag = get_tag(vmajor, vminor, vmicro)
         print('determining version build from {}'.format(tag))
@@ -56,23 +60,24 @@ def update_version():
         # assume if tag does not exist that it has not been added
         except:
             vbuild = 0
-    
+
         v1 = get_version_str(vmajor, vminor, vmicro, vbuild)
-    
+
         # get current build number
         b = subprocess.Popen(("git", "describe", "--match", "build"),
                              stdout=subprocess.PIPE).communicate()[0]
         vcommit = int(b.decode().strip().split('-')[1]) + 2
-    
+
         print('Updating version:')
         print('  ', v0, '->', v1)
-    
+
         # write new version file
         f = open(pth, 'w')
         f.write('# {} version file automatically '.format(pak) +
                 'created using...{0}\n'.format(os.path.basename(__file__)))
         f.write('# created on...' +
-                '{0}\n'.format(datetime.datetime.now().strftime("%B %d, %Y %H:%M:%S")))
+                '{0}\n'.format(
+                    datetime.datetime.now().strftime("%B %d, %Y %H:%M:%S")))
         f.write('\n')
         f.write('major = {}\n'.format(vmajor))
         f.write('minor = {}\n'.format(vminor))
@@ -80,7 +85,8 @@ def update_version():
         f.write('build = {}\n'.format(vbuild))
         f.write('commit = {}\n\n'.format(vcommit))
         f.write("__version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)\n")
-        f.write("__build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)\n")
+        f.write(
+            "__build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)\n")
         f.write("__git_commit__ = '{:d}'.format(commit)\n")
         f.close()
         print('Succesfully updated version.py')
@@ -88,17 +94,69 @@ def update_version():
         print('There was a problem updating the version file')
         sys.exit(1)
 
-def add_updated_version():
+    # update README.md with new version information
+    update_readme_markdown(vmajor, vminor, vmicro, vbuild)
+
+
+def add_updated_files():
     try:
         # add modified version file
         print('Adding updated version file to repo')
-        b = subprocess.Popen(("git", "add", "{}/version.py".format(pak)),
+        fpths = '{}/{}'.format(pak, files[0])
+        fpths += ' {}'.format(files[1])
+        b = subprocess.Popen(("git", "add", fpths),
                              stdout=subprocess.PIPE).communicate()[0]
     except:
         print('Could not add updated version file')
-        sys.exit(1) 
+        sys.exit(1)
+
+
+def update_readme_markdown(vmajor, vminor, vmicro, vbuild):
+    try:
+        # determine current buildstat branch
+        b = subprocess.Popen(("git", "status"),
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT).communicate()[0]
+        if isinstance(b, bytes):
+            b = b.decode('utf-8')
+
+        # determine current buildstat branch
+        for line in b.splitlines():
+            if 'On branch' in line:
+                branch = line.replace('On branch ', '').rstrip()
+    except:
+        print('Cannot update README.md - could not determine current branch')
+        return
+
+    # create version
+    version = get_tag(vmajor, vminor, vmicro)
+
+    # read README.md into memory
+    with open(files[1], 'r') as file:
+        lines = [line.strip() for line in file]
+
+    # rewrite README.md
+    f = open(files[1], 'w')
+    for line in lines:
+        if '### Version ' in line:
+            line = '### Version {}'.format(version)
+            if vbuild > 0:
+                line += ' {} &mdash; build {}'.format(branch, vbuild)
+        elif '[Build Status]' in line:
+            line = '[![Build Status](https://travis-ci.org/modflowpy/' + \
+                   'flopy.svg?branch={})]'.format(branch) + \
+                   '(https://travis-ci.org/modflowpy/flopy)'
+        elif '[Coverage Status]' in line:
+            line = '[![Coverage Status](https://coveralls.io/repos/github/' + \
+                   'modflowpy/flopy/badge.svg?branch={})]'.format(branch) + \
+                   '(https://coveralls.io/github/modflowpy/' + \
+                   'flopy?branch={})'.format(branch)
+        f.write('{}\n'.format(line))
+    f.close()
+
+    return
+
 
 if __name__ == "__main__":
     update_version()
-    add_updated_version()
-    
+    add_updated_files()
