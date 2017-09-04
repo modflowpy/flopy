ea64a759f186a6b592d33a295732a2cb49e52e22
Joseph D Hughes <jdhughes@usgs.gov>
2017-09-04 12:31:47 -0400

    Updated extents for t029 dis grb and updated grb file. Hope this is correct.

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...September 04, 2017 12:07:51
+# created on...September 04, 2017 12:31:47
 
 major = 3
 minor = 2
 micro = 6
-build = 152
-commit = 2224
+build = 153
+commit = 2225
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
