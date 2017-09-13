3964672ff1b496ed581b848fd36b4aae03d49526
Joseph D Hughes <jdhughes@usgs.gov>
2017-09-13 12:10:05 -0700

    Fix and test for https://github.com/modflowpy/flopy/issues/248

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...September 07, 2017 11:21:48
+# created on...September 13, 2017 12:10:05
 
 major = 3
 minor = 2
 micro = 6
-build = 155
-commit = 2227
+build = 156
+commit = 2228
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
