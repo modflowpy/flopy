989e77f0750054bb2f584b7d2db8f0f82bcf0b2e
spaulins-usgs <spaulinski@usgs.gov>
2017-11-15 14:52:22 -0800

    MF6 test cases

---
+++ examples/data/mf6/test005_advgw_tidal/recharge_rates.ts
@@ -0,0 +1,14 @@
+# The ATTRIBUTES block is required.
+BEGIN ATTRIBUTES
+  NAME rch_1 rch_2 rch_3
+  METHODS stepwise linear linear
+END ATTRIBUTES
+
+BEGIN TIMESERIES
+#time   zone-1   zone-2   zone-3
+0.0     0.0015   0.0016   0.0017
+1.0     0.0010   0.0018   0.0020
+11.0    0.0015   0.0019   0.0017
+21.0    0.0025   0.0016   0.0018
+31.0    0.0015   0.0018   0.0020
+END TIMESERIES
