3b880212c6588a3e515aef6c48346226fe973f93
Christian D Langevin <langevin@usgs.gov>
2017-11-16 12:28:04 -0500

    Reworked t503 so that the individual models show up as different tests as reported by nosetests.

--- autotest/t503_test.py
+++ autotest/t503_test.py
@@ -35,65 +35,83 @@ def download_mf6_distribution():
     return mf6path
 
 
+out_dir = os.path.join('temp', 't503')
+if os.path.exists(out_dir):
+    shutil.rmtree(out_dir)
+os.mkdir(out_dir)
+
+mf6path = download_mf6_distribution()
+distpth = os.path.join(mf6path, 'examples')
+folders = [f for f in os.listdir(distpth)
+           if os.path.isdir(os.path.join(distpth, f))]
+
+for f in folders:
+    src = os.path.join(distpth, f)
+    dst = os.path.join(out_dir, f)
+    print('copying {}'.format(f))
+    shutil.copytree(src, dst)
+
+exe_name = 'mf6'
+if platform.system() == 'Windows':
+    exe_name += '.exe'
+v = flopy.which(exe_name)
+run = True
+if v is None:
+    run = False
+
+
+def run_test(f):
+
+    print('\n\n')
+    print('**** RUNNING TEST: {} ****'.format(f))
+    print('\n')
+
+    # load the model into a flopy simulation
+    folder = os.path.join(out_dir, f)
+    print('loading {}'.format(folder))
+    sim = flopy.mf6.MFSimulation.load(f, 'mf6', exe_name, folder)
+    assert isinstance(sim, flopy.mf6.MFSimulation)
+
+    # run the simulation in folder if executable is available
+    if run:
+        success, buff = sim.run_simulation()
+        assert success
+
+        headfiles = [f for f in os.listdir(folder)
+                     if f.lower().endswith('.hds')]
+
+        folder2 = folder + '-RERUN'
+        sim.simulation_data.mfpath.set_sim_path(folder2)
+        sim.write_simulation()
+        success, buff = sim.run_simulation()
+        assert success
+
+        headfiles1 = []
+        headfiles2 = []
+        for hf in headfiles:
+            headfiles1.append(os.path.join(folder, hf))
+            headfiles2.append(os.path.join(folder2, hf))
+
+        outfile = os.path.join(folder, 'head_compare.dat')
+        success = pymake.compare_heads(None, None, precision='double',
+                                       text='head', files1=headfiles1,
+                                       files2=headfiles2, outfile=outfile)
+        assert success
+
+
+# for running tests with nosetests
 def test_load_mf6_distribution_models():
-
-    out_dir = os.path.join('temp', 't503')
-    if os.path.exists(out_dir):
-        shutil.rmtree(out_dir)
-    os.mkdir(out_dir)
-
-    mf6path = download_mf6_distribution()
-    distpth = os.path.join(mf6path, 'examples')
-    folders = [f for f in os.listdir(distpth)
-               if os.path.isdir(os.path.join(distpth, f))]
-
     for f in folders:
-        src = os.path.join(distpth, f)
-        dst = os.path.join(out_dir, f)
-        print('copying {}'.format(f))
-        shutil.copytree(src, dst)
+        yield run_test, f
+    return
 
-    exe_name = 'mf6'
-    if platform.system() == 'Windows':
-        exe_name += '.exe'
-    v = flopy.which(exe_name)
-    run = True
-    if v is None:
-        run = False
 
+# for running outside of nosetests
+def run_tests():
     for f in folders:
+        run_test(f)
+    return
 
-        # load the model into a flopy simulation
-        folder = os.path.join(out_dir, f)
-        print('loading {}'.format(folder))
-        sim = flopy.mf6.MFSimulation.load(f, 'mf6', exe_name, folder)
-        assert isinstance(sim, flopy.mf6.MFSimulation)
-
-        # run the simulation in folder if executable is available
-        if run:
-            success, buff = sim.run_simulation()
-            assert success
-
-            headfiles = [f for f in os.listdir(folder)
-                         if f.lower().endswith('.hds')]
-
-            folder2 = folder + '-RERUN'
-            sim.simulation_data.mfpath.set_sim_path(folder2)
-            sim.write_simulation()
-            success, buff = sim.run_simulation()
-            assert success
-
-            headfiles1 = []
-            headfiles2 = []
-            for hf in headfiles:
-                headfiles1.append(os.path.join(folder, hf))
-                headfiles2.append(os.path.join(folder2, hf))
-
-            outfile = os.path.join(folder, 'head_compare.dat')
-            success = pymake.compare_heads(None, None, precision='double',
-                                           text='head', files1=headfiles1,
-                                           files2=headfiles2, outfile=outfile)
-            assert success
 
 if __name__ == '__main__':
-    test_load_mf6_distribution_models()
+    run_tests()
