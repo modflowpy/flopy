a90c4bc6bd902fd9e279fd3f71b57ae428ef808e
Joseph D Hughes <jdhughes@usgs.gov>
2017-11-28 16:56:13 -0500

    Add symbolic link to gcc-6 for rasterio dependency

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...November 28, 2017 16:45:58
+# created on...November 28, 2017 16:56:13
 
 major = 3
 minor = 2
 micro = 6
-build = 378
-commit = 2450
+build = 379
+commit = 2451
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
