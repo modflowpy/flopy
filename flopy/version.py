c9a96783f0f9496e492147e28423501baafde76c
Joseph D Hughes <jdhughes@usgs.gov>
2017-11-28 17:08:25 -0500

    A few dependencies for rasterio

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...November 28, 2017 16:56:13
+# created on...November 28, 2017 17:08:25
 
 major = 3
 minor = 2
 micro = 6
-build = 379
-commit = 2451
+build = 380
+commit = 2452
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
