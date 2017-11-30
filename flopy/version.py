d062c4ef477cae1749e3dea3c31bad6f8fb29949
Joseph D Hughes <jdhughes@usgs.gov>
2017-11-30 17:37:17 -0500

    Cleanup before release

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...November 30, 2017 16:52:24
+# created on...November 30, 2017 17:37:17
 
 major = 3
 minor = 2
 micro = 6
-build = 399
-commit = 2471
+build = 400
+commit = 2472
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
