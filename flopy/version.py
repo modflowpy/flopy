79600a8bed2bacfe83f15e04fa4f08cef4caec69
Joseph D Hughes <jdhughes@usgs.gov>
2017-12-05 15:46:04 -0600

    Update travis.yml file

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...December 05, 2017 14:09:40
+# created on...December 05, 2017 15:46:04
 
 major = 3
 minor = 2
 micro = 7
-build = 12
-commit = 2488
+build = 13
+commit = 2489
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
