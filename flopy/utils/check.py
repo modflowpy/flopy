0c5682b3b263a2dc471f1c8e1e68fb5f71191600
Mike Toews <m.toews@gns.cri.nz>
2017-12-15 14:06:07 +1300

    Add `has_package(name)` feature to see if a package exists.
    This feature goes nicely beside `get_package(name)` (modified slightly).
    
    Updated/amended other parts of code and a few autotests.

--- flopy/utils/check.py
+++ flopy/utils/check.py
@@ -226,12 +226,12 @@ class check:
     def _stress_period_data_valid_indices(self, stress_period_data):
         """Check that stress period data inds are valid for model grid."""
         spd_inds_valid = True
-        if 'DIS' in self.model.get_package_list() and\
+        if self.model.has_package('DIS') and \
                 {'k', 'i', 'j'}.intersection(set(stress_period_data.dtype.names)) != {'k', 'i', 'j'}:
             self._add_to_summary(type='Error',
                                 desc='\r    Stress period data missing k, i, j for structured grid.')
             spd_inds_valid = False
-        elif 'DISU' in self.model.get_package_list() and \
+        elif self.model.has_package('DISU') and \
                         'node' not in stress_period_data.dtype.names:
             self._add_to_summary(type='Error',
                                 desc='\r    Stress period data missing node number for unstructured grid.')
