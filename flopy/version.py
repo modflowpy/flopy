db4a09de4870b45859533df40ac50079fb03989a
Joseph D Hughes <jdhughes@usgs.gov>
2017-12-18 15:19:15 -0500

    A few changes to files on develop

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...December 18, 2017 15:17:44
+# created on...December 18, 2017 15:19:15
 
 major = 3
 minor = 2
 micro = 8
-build = 0
-commit = 2520
+build = 2
+commit = 2521
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
