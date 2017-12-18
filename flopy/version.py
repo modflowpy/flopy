86b3f2fb3866a71a06360c912618ffa2c704695c
Joseph D Hughes <jdhughes@usgs.gov>
2017-12-18 15:17:44 -0500

    Merge 3.2.8 into develop

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...December 18, 2017 15:00:01
+# created on...December 18, 2017 15:17:44
 
 major = 3
 minor = 2
 micro = 8
 build = 0
-commit = 2519
+commit = 2520
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
