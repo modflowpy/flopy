b05e81100c850ba486d293268e1b0f24c639e071
Joseph D Hughes <jdhughes@usgs.gov>
2018-01-05 17:41:28 -0500

    add createpackages.py to MANIFEST.in for travis builds

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...January 05, 2018 12:53:15
+# created on...January 05, 2018 17:41:28
 
 major = 3
 minor = 2
 micro = 8
-build = 19
-commit = 2538
+build = 21
+commit = 2540
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
