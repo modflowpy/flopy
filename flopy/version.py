196770f4dfd9435db8c494589561b12696a8dc0d
Joseph D Hughes <jdhughes@usgs.gov>
2018-01-08 16:31:20 -0500

    fixed issue with default settings for SUB dp dataset. Also removed createpackages.py from MANIFEST.in (does not seem to be necessary)

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...January 05, 2018 17:41:28
+# created on...January 08, 2018 16:31:20
 
 major = 3
 minor = 2
 micro = 8
-build = 21
-commit = 2540
+build = 24
+commit = 2543
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
