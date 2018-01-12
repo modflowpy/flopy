a5f9f1632edd0be6b9c249daab031f37f2383718
Joseph D Hughes <jdhughes@usgs.gov>
2018-01-12 18:32:17 -0500

    Write one-based stress period comments to MODFLOW-2005 models

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...January 08, 2018 16:31:20
+# created on...January 12, 2018 18:32:17
 
 major = 3
 minor = 2
 micro = 8
-build = 24
-commit = 2543
+build = 39
+commit = 2558
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
