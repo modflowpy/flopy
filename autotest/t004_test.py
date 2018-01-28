71ac6702cecf8682b1b6da0b6e5671891b364ad1
Jeremy White <jtwhite1000@gmail.com>
2018-01-29 09:46:25 +1300

    added a function to demo the issue with a single entry in an external BC list.  Left it as a function rather than a test until a solution is found

--- autotest/t004_test.py
+++ autotest/t004_test.py
@@ -584,6 +584,24 @@ def test_util3d_reset():
     arr = np.ones((ml.nlay, ml.nrow, ml.ncol))
     ml.bas6.strt = arr
 
+def single_mflist_entry_load_issue():
+
+    import os
+    import numpy as np
+    import flopy
+
+    model_ws = os.path.join("..","examples","data","freyberg")
+    m = flopy.modflow.Modflow.load("freyberg.nam",model_ws=model_ws,load_only=["WEL"],check=False)
+    w = m.wel
+    spd = w.stress_period_data
+    print(spd[0])
+    flopy.modflow.ModflowWel(m,stress_period_data={0:[0,0,0,0.0]})
+    m.external_path = "."
+    m.change_model_ws("temp",reset_external=True)
+    m.write_input()
+
+    mm = flopy.modflow.Modflow.load("freyberg.nam", model_ws="temp",forgive=False)
+    print(mm.wel.stess_period_data[0])
 
 if __name__ == '__main__':
     # test_util3d_reset()
@@ -599,7 +617,8 @@ if __name__ == '__main__':
     # test_util2d_external_fixed_nomodelws()
     # test_util2d_external_fixed_path_nomodelws()
     # test_transient2d()
-    test_transient3d()
+    #test_transient3d()
+    single_mflist_entry_load_issue()
     # test_util2d()
     # test_util3d()
     # test_how()
