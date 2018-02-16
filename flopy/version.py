148525b2c17c66028d16ece32e184ce644cd09bb
Joseph D Hughes <jdhughes@usgs.gov>
2018-02-16 13:50:17 -0500

    Restrict notebook and script autotests to python 3.6

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...February 16, 2018 10:58:29
+# created on...February 16, 2018 13:50:18
 
 major = 3
 minor = 2
 micro = 8
-build = 131
-commit = 2650
+build = 138
+commit = 2657
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
