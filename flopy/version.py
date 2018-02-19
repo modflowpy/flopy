25392dcfe735512a4a6a061819919f1bd0d3c681
Joseph D Hughes <jdhughes@usgs.gov>
2018-02-19 11:22:41 -0500

    Last commit to develop before 3.2.9

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...February 16, 2018 17:27:40
+# created on...February 19, 2018 11:22:41
 
 major = 3
 minor = 2
 micro = 8
-build = 150
-commit = 2669
+build = 151
+commit = 2670
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
