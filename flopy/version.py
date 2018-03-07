1f5a551e4c7d73fe38ea203b26a4714ff596fab1
Joseph D Hughes <jdhughes@usgs.gov>
2018-03-07 09:11:57 -0500

    Updated version_changes.md

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...March 06, 2018 17:40:31
+# created on...March 07, 2018 09:11:58
 
 major = 3
 minor = 2
 micro = 9
-build = 28
-commit = 2699
+build = 35
+commit = 2706
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
