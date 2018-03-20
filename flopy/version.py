c8498fdabaacf3955fcc7ae5fd6fae608bd25a00
Joseph D Hughes <jdhughes@usgs.gov>
2018-03-20 16:43:29 -0400

    Update to mflist to read mf6 list files

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...March 20, 2018 16:40:48
+# created on...March 20, 2018 16:43:29
 
 major = 3
 minor = 2
 micro = 9
-build = 54
-commit = 2725
+build = 55
+commit = 2726
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
