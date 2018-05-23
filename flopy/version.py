0530d8251a18795947101687a0b05e85a84a7501
Joseph D Hughes <jdhughes@usgs.gov>
2018-05-23 11:45:18 -0400

    Attempting to get notebooks to run without appending flopy to path

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...May 23, 2018 09:39:55
+# created on...May 23, 2018 11:45:19
 
 major = 3
 minor = 2
 micro = 9
-build = 131
-commit = 2802
+build = 132
+commit = 2803
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
