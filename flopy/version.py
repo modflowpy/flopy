c4aec4dad5a715810283be1f904ee2684746123b
Joseph D Hughes <jdhughes@usgs.gov>
2018-05-24 14:01:06 -0400

    Fixed issue with FAQ notebooks

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...May 24, 2018 13:50:23
+# created on...May 24, 2018 14:01:07
 
 major = 3
 minor = 2
 micro = 9
-build = 135
-commit = 2806
+build = 136
+commit = 2807
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
