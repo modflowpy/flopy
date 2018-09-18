c076aab15fa21e6be7c4fa17350b8d23343efd27
jdhughes-usgs <jdhughes@usgs.gov>
2018-09-17 19:41:52 -0700

    fix(util2d): Create binary integer array files
    Remove conditional that prevented integer arrays from being written
    as binary files.

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...August 31, 2018 18:38:18
+# created on...September 17, 2018 19:41:52
 
 major = 3
 minor = 2
 micro = 9
-build = 186
-commit = 2857
+build = 199
+commit = 2870
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
