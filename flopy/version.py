e3eef99e537cd84f148471c681bf0ff77837ef26
jdhughes-usgs <jdhughes@usgs.gov>
2018-10-16 18:38:27 -0400

    ci(travis): update travis.yml

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...October 16, 2018 18:19:22
+# created on...October 16, 2018 18:38:27
 
 major = 3
 minor = 2
 micro = 9
-build = 216
-commit = 2887
+build = 217
+commit = 2888
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
