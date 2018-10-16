3421aa13abadad571aa376704ab68f43e405af10
jdhughes-usgs <jdhughes@usgs.gov>
2018-10-16 16:35:33 -0400

    docs: update documentation
    Update docstrings for MFGridFile class. Update README.md and
    CONTRIBUTING.md. Update pre-commit.py to use develop branch for
    branches with "-" or ":" in the branch name (it is assumed these
    are bug fix or feature branches for the develop branch.

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...October 13, 2018 20:06:37
+# created on...October 16, 2018 16:35:33
 
 major = 3
 minor = 2
 micro = 9
-build = 215
-commit = 2886
+build = 214
+commit = 2885
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
