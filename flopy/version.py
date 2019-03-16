ac526a0d6bc7fecfbc3ac7f3b15bdb93c97459f7
Hughes, J.D <jdhughes@usgs.gov>
2019-03-16 10:30:13 -0400

    refactor(autotests): update build_exe.py to use pymake updates (#484)
    update build_exe.py to use pymake updates. Define environments so notebooks 
    and scripts are run in a separate environment with python 3.7 to prevent travis 
    timeouts.

--- flopy/version.py
+++ flopy/version.py
@@ -1,11 +1,11 @@
 # flopy version file automatically created using...pre-commit.py
-# created on...October 19, 2018 18:28:49
+# created on...March 15, 2019 16:43:51
 
 major = 3
 minor = 2
 micro = 10
-build = 0
-commit = 2892
+build = 60
+commit = 2954
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
