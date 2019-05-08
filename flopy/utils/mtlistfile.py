5271bd443ec575d1f765912ab42dc7cb9aa802da
Mike Taves <mwtoews@gmail.com>
2019-05-08 22:27:52 +1200

    refactor warnings
     * catch intentional warnings in tests shown with "default" filter
     * change PendingDeprecationWarning -> DeprecationWarning for
       "xul/yul have been deprecated" (this was already done for this
       message in other modules)
     * MtListBudget: file was opened twice (shows ResourceWarning)
     * fix numpy FutureWarning

--- flopy/utils/mtlistfile.py
+++ flopy/utils/mtlistfile.py
@@ -35,15 +35,7 @@ class MtListBudget(object):
         Class constructor
         """
 
-        # Set up file reading
-        assert os.path.exists(file_name), "file_name {0} not found".format(
-            file_name)
         self.file_name = file_name
-        if sys.version_info[0] == 2:
-            self.f = open(file_name, 'r')
-        elif sys.version_info[0] == 3:
-            self.f = open(file_name, 'r', encoding='ascii', errors='replace')
-
         self.tssp_lines = 0
 
         # Assign the budgetkey, which should have been overridden
