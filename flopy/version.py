b80a89c18821fe1bc7375c15e4c03d1e0be70916
Hughes, J.D <jdhughes@usgs.gov>
2020-05-26 17:54:27 -0400

    fix(setup.py): read package name, version, etc. from version.py (#893)
    Import of `__version__`, `__name__`, and `__author__` from `flopy` caused
    failure of `setup.py` because of dependency of `flopy` on `numpy` with
    python installations that did not already have `numpy` installed. Moved
    all information needed by `setup.py` to `version.py` and update
    `make-release.py` to retain `__name__`, `__author__`, and `__author_email__`
    information in file after updating version numbers. Removed unintended
    unmanaged dependency on `matplotlib` in `gridintersect.py` by wrapping
    import in a try and except. In the case that `matplotlib` is not installed
    and methods that depend on `matplotlib` are called then a
    `ModuleNotFoundError` exception is issued.
    
    Closes #892
    
    * fix(setup.py): read package name, version, etc. from version.py
    
    Import of __version__, __name__, and __author__ from flopy caused
    failure of setup.py because of dependency of flopy on numpy with
    python installations that did not already have numpy installed. Moved
    all information needed by setup.py to version.py and update
    make-release.py to retain __name__, __author__, and __author_email__
    information in file after updating version numbers. Removed unintended
    unmanaged dependency on matplotlib in gridintersect.py by wrapping
    import in a try and except. In the case that matplotlib is not installed
    and methods that depend on matplotlib are called then a
    ModuleNotFoundError exception is issued.
    
    Closes #892

--- flopy/version.py
+++ flopy/version.py
@@ -1,7 +1,28 @@
 # flopy version file automatically created using...make-release.py
-# created on...December 15, 2019 09:55:43
+# created on...May 26, 2020 17:16:58
 
 major = 3
 minor = 3
 micro = 1
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
+
+__pakname__ = 'flopy'
+
+# edit author dictionary as necessary
+author_dict = {
+    'Mark Bakker': 'mark.bakker@tudelft.nl',
+    'Vincent Post': 'Vincent.Post@bgr.de',
+    'Christian D. Langevin': 'langevin@usgs.gov',
+    'Joseph D. Hughes': 'jdhughes@usgs.gov',
+    'Jeremy T. White': 'jwhite@usgs.gov',
+    'Andrew T. Leaf': 'aleaf@usgs.gov',
+    'Scott R. Paulinski': 'spaulinski@usgs.gov',
+    'Joshua D. Larsen': 'jlarsen@usgs.gov',
+    'Michael W. Toews': 'M.Toews@gns.cri.nz',
+    'Eric D. Morway': 'emorway@usgs.gov',
+    'Jason C. Bellino': 'jbellino@usgs.gov',
+    'Jeffrey Starn': 'jjstarn@usgs.gov',
+    'Michael N. Fienen': 'mnfienen@usgs.gov',
+}
+__author__ = ', '.join(author_dict.keys())
+__author_email__ = ', '.join(s for _, s in author_dict.items())
--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ import os
 import sys
 from setuptools import setup
 
-from flopy import __version__, __name__, __author__
-
 # ensure minimum version of Python is running
 if sys.version_info[0:2] < (3, 5):
     raise RuntimeError('Flopy requires Python >= 3.5')
+
+# local import of package variables in flopy/version.py
+sys.path.append('flopy')
+from version import __version__, __pakname__, __author__, __author_email__
+print(__version__, __pakname__, __author__, __author_email__)
 
 try:
     import pypandoc
@@ -16,18 +19,12 @@ try:
 except ImportError:
     long_description = ''
 
-setup(name=__name__,
+setup(name=__pakname__,
       description='FloPy is a Python package to create, run, and ' +
                   'post-process MODFLOW-based models.',
       long_description=long_description,
       author=__author__,
-      author_email='mark.bakker@tudelft.nl, Vincent.Post@bgr.de, ' +
-                   'langevin@usgs.gov, jdhughes@usgs.gov, ' +
-                   'j.white@gns.cri.nz, aleaf@usgs.gov, ' +
-                   'spaulinski@usgs.gov, jlarsen@usgs.gov,' +
-                   'M.Toews@gns.cri.nz, emorway@usgs.gov, ' +
-                   'jbellino@usgs.gov, jjstarn@usgs.gov, ' +
-                   'mnfienen@usgs.gov',
+      author_email=__author_email__,
       url='https://github.com/modflowpy/flopy/',
       license='CC0',
       platforms='Windows, Mac OS-X, Linux',
