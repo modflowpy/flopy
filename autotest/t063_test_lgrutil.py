8005effe26c77bba32b56b61adb24c2fe3fa51a1
Christian Langevin <langevin@usgs.gov>
2020-07-27 12:38:09 -0500

    fix(lgrutil): corrected bug in child to parent vertical exchanges

--- autotest/t063_test_lgrutil.py
+++ autotest/t063_test_lgrutil.py
@@ -45,28 +45,37 @@ def test_lgrutil():
     assert ac[0, 0] == 6
     assert ac[-1, -1] == 18
 
-    # top/bottom
-    top, botm = lgr.get_top_botm()
-    assert top.shape == (9, 9)
-    assert botm.shape == (2, 9, 9)
-    assert top.min() == top.max() == 100.
-    assert botm.min() == botm.max() == 0.
+    # child top/bottom
+    topc, botmc = lgr.get_top_botm()
+    assert topc.shape == (9, 9)
+    assert botmc.shape == (2, 9, 9)
+    assert topc.min() == topc.max() == 100.
+    errmsg = '{} /= {}'.format(botmc[:, 0, 0], np.array(botmp[:2]))
+    assert np.allclose(botmc[:, 0, 0], np.array(botmp[:2])), errmsg
 
     # exchange data
     exchange_data = lgr.get_exchange_data(angldegx=True, cdist=True)
+
+
     ans1 = [(0, 1, 0), (0, 0, 0), 1, 50.0, 16.666666666666668,
             33.333333333333336, 0.0, 354.33819375782156]
-    ans2 = [(1, 4, 3), (1, 8, 8), 1, 50.0, 16.666666666666668,
-            33.333333333333336, 90.0, 354.3381937578216]
-    assert exchange_data[0] == ans1
-    assert exchange_data[-1] == ans2
-    assert len(exchange_data) == 72
+    errmsg = '{} /= {}'.format(ans1, exchange_data[0])
+    assert exchange_data[0] == ans1, errmsg
+
+    ans2 = [(2, 3, 3), (1, 8, 8), 0, 50.0, 50,
+            1111.1111111111113, 180., 100.]
+    errmsg = '{} /= {}'.format(ans2, exchange_data[-1])
+    assert exchange_data[-1] == ans2, errmsg
+
+    errmsg = 'exchanges should be 71 horizontal plus 81 vertical'
+    assert len(exchange_data) == 72 + 81, errmsg
 
     # list of parent cells connected to a child cell
     assert lgr.get_parent_connections(0, 0, 0) == [((0, 1, 0), -1),
                                                    ((0, 0, 1), 2)]
     assert lgr.get_parent_connections(1, 8, 8) == [((1, 3, 4), 1),
-                                                   ((1, 4, 3), -2)]
+                                                   ((1, 4, 3), -2),
+                                                   ((2, 3, 3), -3)]
 
     return
 
