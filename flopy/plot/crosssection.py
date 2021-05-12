be229b68f3411b27c202c9f98887cfb6d1fd485d
Joshua Larsen <jlarsen@usgs.gov>
2021-05-12 12:59:35 -0700

    fix(plot_pathline): update modpath intersection routines for multilayered grids
    * added test_pathline_plot_xc to t049_test.py

--- flopy/plot/crosssection.py
+++ flopy/plot/crosssection.py
@@ -631,7 +631,7 @@ class PlotCrossSection:
             else:
                 ibound = self.mg.idomain
 
-        plotarray = np.zeros(ibound.shape, dtype=int)
+        plotarray = np.zeros(ibound.shape, dtype=np.int)
         idx1 = ibound == 0
         plotarray[idx1] = 1
         plotarray = np.ma.masked_equal(plotarray, 0)
@@ -685,7 +685,7 @@ class PlotCrossSection:
 
             ibound = self.mg.idomain
 
-        plotarray = np.zeros(ibound.shape, dtype=int)
+        plotarray = np.zeros(ibound.shape, dtype=np.int)
         idx1 = ibound == 0
         idx2 = ibound < 0
         plotarray[idx1] = 1
@@ -823,11 +823,11 @@ class PlotCrossSection:
                     idx = mflist["node"]
 
         if len(self.mg.shape) != 3:
-            plotarray = np.zeros((self._nlay, self._ncpl), dtype=int)
+            plotarray = np.zeros((self._nlay, self._ncpl), dtype=np.int)
             plotarray[tuple(idx)] = 1
         else:
             plotarray = np.zeros(
-                (self.mg.nlay, self.mg.nrow, self.mg.ncol), dtype=int
+                (self.mg.nlay, self.mg.nrow, self.mg.ncol), dtype=np.int
             )
             plotarray[idx[0], idx[1], idx[2]] = 1
 
@@ -1155,7 +1155,7 @@ class PlotCrossSection:
             # thickness by setting laytyp to zeros
             if head is None or laytyp is None:
                 head = np.zeros(botm.shape, np.float32)
-                laytyp = np.zeros((nlay,), dtype=int)
+                laytyp = np.zeros((nlay,), dtype=np.int)
                 head[0, :, :] = top
                 if nlay > 1:
                     head[1:, :, :] = botm[:-1, :, :]
@@ -1170,7 +1170,7 @@ class PlotCrossSection:
             )
 
             if qz is None:
-                qz = np.zeros(qx.shape, dtype=float)
+                qz = np.zeros(qx.shape, dtype=np.float)
 
             qx = qx.ravel()
             qy = qy.ravel()
@@ -1258,6 +1258,7 @@ class PlotCrossSection:
             self.xvertices,
             self.yvertices,
             self.direction,
+            self._ncpl,
             method=method,
         )
         plines = plotutil.reproject_modpath_to_crosssection(
@@ -1266,6 +1267,7 @@ class PlotCrossSection:
             self.xypts,
             self.direction,
             self.mg,
+            self._ncpl,
             self.geographic_coords,
         )
 
