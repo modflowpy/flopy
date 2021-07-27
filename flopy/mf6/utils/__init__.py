8360c066ae3ddc696a7c17598482da7641d7fe91
Joseph D Hughes <jdhughes@usgs.gov>
2021-07-27 17:30:16 -0500

    feat(mf6-lake): add helper function to create lake connections

--- flopy/mf6/utils/__init__.py
+++ flopy/mf6/utils/__init__.py
@@ -3,3 +3,4 @@ from . import createpackages
 from .generate_classes import generate_classes
 from .binarygrid_util import MfGrdFile
 from .postprocessing import get_structured_faceflows, get_residuals
+from .lakpak_utils import get_lak_connections
