02b7622f412a158dd408ad1c3d107520f2b1e9ca
Joshua Larsen <jlarsen@usgs.gov>
2022-04-14 17:46:22 -0700

    update(__zonebudget): add "gwt" support to .output.zonebuget()

--- flopy/mf6/utils/output_util.py
+++ flopy/mf6/utils/output_util.py
@@ -255,7 +255,7 @@ class MF6Output:
             ZoneFile6(zonbud, izone)
             zonbud.bud = budget
             try:
-                if self._obj.model_or_sim.model_type in ("gwf", "gwf6"):
+                if "gwf" in self._obj.model_or_sim.model_type or "gwt" in self._obj.model_or_sim.model_type:
                     if self._obj.package_type == "oc":
                         dis = self._obj.model_or_sim.dis
                         if (
