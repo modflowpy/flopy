51d0bec0d79fe36d6b7711524db422e2828f2a22
Joshua Larsen <jlarsen@usgs.gov>
2022-04-14 17:48:19 -0700

    linting

--- flopy/mf6/utils/output_util.py
+++ flopy/mf6/utils/output_util.py
@@ -255,7 +255,10 @@ class MF6Output:
             ZoneFile6(zonbud, izone)
             zonbud.bud = budget
             try:
-                if "gwf" in self._obj.model_or_sim.model_type or "gwt" in self._obj.model_or_sim.model_type:
+                if (
+                    "gwf" in self._obj.model_or_sim.model_type
+                    or "gwt" in self._obj.model_or_sim.model_type
+                ):
                     if self._obj.package_type == "oc":
                         dis = self._obj.model_or_sim.dis
                         if (
