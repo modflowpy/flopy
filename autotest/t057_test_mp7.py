cae9d27c2cdeb83c281138cb0b0e464c5ebda62d
rodrperezi <rodrperez@ing.uchile.cl>
2022-05-05 14:11:49 -0400

    fix(mp7-releaseinterval) unnecessary int type when releaseoption 2 (#1393)
    - flopy is forcing int type for releaseinterval and modpath allows float
    - although it doesn't generate further problems, current releasetimes is not consistent with releaseoption definition
    - end of releasetimes arange from starting releasetime
    - verify consistent releasetimes for all releaseoptions and releaseinterval type for releaseoption 2
    
    Co-authored-by: rodrperezi <rodrperezi@gmail.com>
    
    closes #1342

--- autotest/t057_test_mp7.py
+++ autotest/t057_test_mp7.py
@@ -150,6 +150,51 @@ def test_endpoint_output():
     return
 
 
+def test_pgroup_release_data():
+    model_ws = f"{base_dir}_test_pgroup_release_data"
+    test_setup = FlopyTestSetup(verbose=True, test_dirs=model_ws)
+
+    # create particles
+    partlocs = []
+    partids  = []
+    for i in range(nrow):
+        partlocs.append((0, i, 2))
+        partids.append(i)
+    pdata = flopy.modpath.ParticleData(
+        partlocs, structured=True, particleids=partids
+    )
+    pgrd1 = flopy.modpath.ParticleGroup(
+        particlegroupname="PG1", particledata=pdata, filename="exrd1.sloc", 
+        releasedata=0.0,
+    )
+    nripg2= 10
+    ripg2 = 1.0
+    pgrd2 = flopy.modpath.ParticleGroup(
+        particlegroupname="PG2", particledata=pdata, filename="exrd2.sloc",
+        releasedata=[nripg2, 0.0, ripg2]
+    )
+    nripg3= 10
+    pgrd3 = flopy.modpath.ParticleGroup(
+        particlegroupname="PG3", particledata=pdata, filename="exrd3.sloc",
+        releasedata=[nripg3, np.arange(0,nripg3)]
+    )
+
+    assert len(pgrd1.releasetimes)     == 1,  \
+            f"mp7: pgroup with releaseoption 1 returned " \
+            f"len(releasetimes)={len(pgrd1.releasetimes)}. Should be 1"
+    assert len(pgrd2.releasetimes)     == nripg2, \
+            f"mp7: pgroup with releaseoption 2 returned " \
+            f"len(releasetimes)={len(pgrd2.releasetimes)}. Should be {nripg2}"
+    assert type(pgrd2.releaseinterval) == type(ripg2),    \
+            f"mp7: pgroup with releaseoption 2 returned " \
+            f"type(releaseinterval)={type(pgrd2.releaseinterval)}. Should remain as {type(ripg2)}"
+    assert len(pgrd3.releasetimes)     == nripg3, \
+            f"mp7: pgroup with releaseoption 3 returned " \
+            f"len(releasetimes)={len(pgrd3.releasetimes)}. Should be {nripg3}"
+
+    return
+
+
 def build_mf2005(model_ws):
     """
     MODPATH 7 example 1 for MODFLOW-2005
@@ -398,4 +443,5 @@ def test_pathline_plotting():
 if __name__ == "__main__":
     test_pathline_output()
     test_endpoint_output()
+    test_pgroup_release_data()
     test_pathline_plotting()
