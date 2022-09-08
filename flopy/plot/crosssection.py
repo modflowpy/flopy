05c3f8201c743a56e2b85b3bb03b6d8e427c81aa
Joshua Larsen <jlarsen@usgs.gov>
2022-09-08 10:06:27 -0700

    linting

--- flopy/plot/crosssection.py
+++ flopy/plot/crosssection.py
@@ -180,10 +180,12 @@ class PlotCrossSection:
 
         if len(self.xypts) < 2:
             if len(list(self.xypts.values())[0]) < 2:
-                s = "cross-section cannot be created\n." \
-                    " less than 2 points intersect the model grid\n" \
-                    f" {len(self.xypts.values()[0])} points" \
-                    f" intersect the grid."
+                s = (
+                    "cross-section cannot be created\n."
+                    " less than 2 points intersect the model grid\n"
+                    f" {len(self.xypts.values()[0])} points"
+                    " intersect the grid."
+                )
                 raise Exception(s)
 
         if self.geographic_coords:
