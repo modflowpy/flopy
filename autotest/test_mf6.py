3b2800e720a786250c00d822e01d4fd2669140a0
Wes Kitlasten <wkitlasten@gmail.com>
2023-02-04 09:50:31 +1300

    fix(datautil): fix SFR connection file parsing (#1694)
    Co-authored-by: Wes Kitlasten <wkitlasten@gmail.com>
    Co-authored-by: Scott Paulinski <spaulinski@usgs.gov>

--- autotest/test_mf6.py
+++ autotest/test_mf6.py
@@ -1031,7 +1031,78 @@ def test_output_add_observation(function_tmpdir, example_data_path):
         sfr_obs, Mf6Obs
     ), "remove and add observation test (Mf6Output) failed"
 
+def test_sfr_connections(function_tmpdir, example_data_path):
+    '''MODFLOW just warns if any reaches are unconnected
+    flopy fails to load model if reach 1 is unconnected, fine with other unconnected'''
+    data_path = str(example_data_path / "mf6" / "test666_sfrconnections")
+    sim_ws = str(function_tmpdir)
+    for test in ['sfr0', 'sfr1']:
+        sim_name = "test_sfr"
+        model_name = "test_sfr"
+        tdis_name = "{}.tdis".format(sim_name)
+        sim = MFSimulation(
+            sim_name=sim_name, version="mf6", exe_name="mf6", sim_ws=sim_ws
+        )
+        tdis_rc = [(1.0, 1, 1.0)]
+        tdis = ModflowTdis(sim, time_units="DAYS", nper=1, perioddata=tdis_rc)
+        ims_package = ModflowIms(
+            sim,
+            pname="my_ims_file",
+            filename=f"{sim_name}.ims",
+            print_option="ALL",
+            complexity="SIMPLE",
+        )
+        model = ModflowGwf(
+            sim, modelname=model_name, model_nam_file="{}.nam".format(model_name)
+        )
 
+        dis = ModflowGwfdis(
+            model,
+            length_units="FEET",
+            nlay=1,
+            nrow=5,
+            ncol=5,
+            delr=5000.0,
+            delc=5000.0,
+            top=100.0,
+            botm=-100.0,
+            filename=f"{model_name}.dis",
+        )
+        ic_package = ModflowGwfic(model, filename=f"{model_name}.ic")
+        npf_package = ModflowGwfnpf(
+            model,
+            pname="npf",
+            save_flows=True,
+            alternative_cell_averaging="logarithmic",
+            icelltype=1,
+            k=50.0,
+        )
+        
+        cnfile = f'mf6_{test}_connection.txt'
+        pkfile = f'mf6_{test}_package.txt'
+
+        with open(os.path.join(data_path, pkfile), 'r') as f:
+            nreaches = len(f.readlines())
+        sfr = ModflowGwfsfr(model,
+                            packagedata={'filename': os.path.join(data_path, pkfile)},
+                            connectiondata={'filename': os.path.join(data_path, cnfile)},
+                            nreaches=nreaches,
+                            pname='sfr',
+                            unit_conversion=86400
+                            )
+        sim.set_all_data_external()
+        sim.write_simulation()
+        success, buff = sim.run_simulation()
+        assert success, f"simulation {sim.name} did not run"
+
+        #reload simulation
+        sim2 = MFSimulation.load(sim_ws=sim_ws)
+        sim.set_all_data_external()
+        sim.write_simulation()
+        success, buff = sim.run_simulation()
+        assert success, f"simulation {sim.name} did not run after being reloaded"
+        
+        
 @requires_exe("mf6")
 def test_array(function_tmpdir):
     # get_data
