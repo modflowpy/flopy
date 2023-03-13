db5a161365bb49aed026e9b2e00fc6b693dc449b
scottrp <45947939+scottrp@users.noreply.github.com>
2023-03-13 10:55:28 -0700

    Update __init__.py

--- flopy/mf6/utils/flopy_plugins/__init__.py
+++ flopy/mf6/utils/flopy_plugins/__init__.py
@@ -1,4 +1,4 @@
 # imports
 from .plugin_interface import FPBMIPluginInterface
 from .plugin_template import generate_plugin_template, create_python_package
-from . import plugins
+from . import plugins  # isort:skip
