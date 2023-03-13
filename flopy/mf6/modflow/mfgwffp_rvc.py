57076f93c0f69a384b277f07620fe49ee3b2a655
scottrp <45947939+scottrp@users.noreply.github.com>
2023-03-13 12:10:03 -0700

    feat(flopy plugins)

--- flopy/mf6/modflow/mfgwffp_rvc.py
+++ flopy/mf6/modflow/mfgwffp_rvc.py
@@ -120,7 +120,7 @@ class ModflowGwffp_Rvc(mfpackage.MFPackage):
         stress_period_data=None,
         filename=None,
         pname=None,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(
             model, "fp_rvc", filename, pname, loading_package, **kwargs
