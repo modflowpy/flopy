48fc8bb6c5cecde2f0389002323b1ee279f4b4cc
Leaf, Andrew T <aleaf@usgs.gov>
2023-04-27 12:30:14 -0500

    docs(examples/Notebooks): draft examples gallery * add filter to .gitattributes to strip Jupyter Notebook output when committing * move examples/Notebooks to .docs/Notebooks; strip output; repath references to examples/data * replace Jupyter Notebooks section of docs with Examples Gallery of thumbnails * add nbsphinx_prolog to conf.py to render source file and Binder link at the top of each example * edit notebook headers to better represent contents in thumbnail label

--- .docs/conf.py
+++ .docs/conf.py
@@ -135,7 +135,6 @@ extensions = [
     "IPython.sphinxext.ipython_console_highlighting",  # lowercase didn't work
     "sphinx.ext.autosectionlabel",
     "nbsphinx",
-    "sphinx_gallery.load_style",
     "nbsphinx_link",
     "recommonmark",
 ]
@@ -276,3 +275,8 @@ nbsphinx_prolog = r"""
 
         __ https://github.com/modflowpy/flopy/blob/develop/.docs/{{ docname }}
 """
+
+# Example gallery thumbnails from static files
+nbsphinx_thumbnails = {
+    "Notebooks/flopy3_mt3d-usgs_example_with_sft_lkt_uzt.ipynb": "https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/thumbnails/image/mt3dms_1.png",
+}
--- .docs/main.rst
+++ .docs/main.rst
@@ -6,7 +6,7 @@
 FloPy Documentation
 ===================
 
-**Documentation for version 3.3.5 --- release candidate**
+**Documentation for version 3.3.7 --- release candidate**
 
 Return to the Github `FloPy <https://github.com/modflowpy/flopy>`_ website where the code resides.
 
@@ -44,4 +44,3 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
-
