7014486cf39abf3569cce450f8f6d2b5df052d09
w-bonelli <wbonelli@uga.edu>
2023-06-29 10:57:52 -0400

    ci(release): update version to 3.dev4

--- flopy/version.py
+++ flopy/version.py
@@ -1,3 +1,3 @@
-# flopy version file automatically created using update_version.py on June 29, 2023 14:20:34
+# flopy version file automatically created using update_version.py on June 29, 2023 10:56:39
 
-__version__ = "3.4.1"
+__version__ = "3.dev4"
