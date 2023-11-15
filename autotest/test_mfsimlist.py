dc29231b386bd02223c24bad4a7820d3270a12e1
mjr-deltares <martijn.russcher@deltares.nl>
2023-11-15 15:37:10 +0100

    too early for that assert

--- autotest/test_mfsimlist.py
+++ autotest/test_mfsimlist.py
@@ -44,23 +44,22 @@ def test_mfsimlist_runtime(function_tmpdir):
     for sim_timer in ("elapsed", "formulate", "solution"):
         runtime_sec = mfsimlst.get_runtime(simulation_timer=sim_timer)
 
-        assert not np.isnan(runtime_sec)
+        if not np.isnan(runtime_sec):
+            runtime_min = mfsimlst.get_runtime(
+                units="minutes", simulation_timer=sim_timer
+            )
+            assert runtime_sec / 60.0 == runtime_min, (
+                f"model {sim_timer} time conversion from "
+                + "sec to minutes does not match"
+            )
 
-        runtime_min = mfsimlst.get_runtime(
-            units="minutes", simulation_timer=sim_timer
-        )
-        assert runtime_sec / 60.0 == runtime_min, (
-            f"model {sim_timer} time conversion from "
-            + "sec to minutes does not match"
-        )
-
-        runtime_hrs = mfsimlst.get_runtime(
-            units="hours", simulation_timer=sim_timer
-        )
-        assert runtime_min / 60.0 == runtime_hrs, (
-            f"model {sim_timer} time conversion from "
-            + "minutes to hours does not match"
-        )
+            runtime_hrs = mfsimlst.get_runtime(
+                units="hours", simulation_timer=sim_timer
+            )
+            assert runtime_min / 60.0 == runtime_hrs, (
+                f"model {sim_timer} time conversion from "
+                + "minutes to hours does not match"
+            )
 
 
 @requires_exe("mf6")
