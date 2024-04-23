bb5461ba32a2047d7451ca53d96ab1643f47309b
langevin-usgs <langevin@usgs.gov>
2024-04-23 16:52:08 -0500

    fix(cvfdutil): polygon area and centroid calculations now use shapely (#2165)

--- flopy/utils/cvfdutil.py
+++ flopy/utils/cvfdutil.py
@@ -1,38 +1,22 @@
 import numpy as np
 
+from .utl_import import import_optional_dependency
+
 
 def area_of_polygon(x, y):
-    """Calculates the signed area of an arbitrary polygon given its vertices
-    https://stackoverflow.com/a/4682656/ (Joe Kington)
-    http://softsurfer.com/Archive/algorithm_0101/algorithm_0101.htm#2D%20Polygons
-    """
-    area = 0.0
-    for i in range(-1, len(x) - 1):
-        area += x[i] * (y[i + 1] - y[i - 1])
-    return area / 2.0
+    shapely = import_optional_dependency("shapely")
+    from shapely.geometry import Polygon
+
+    pgon = Polygon(zip(x, y))
+    return pgon.area
 
 
 def centroid_of_polygon(points):
-    """
-    https://stackoverflow.com/a/14115494/ (mgamba)
-    """
-    import itertools as IT
+    shapely = import_optional_dependency("shapely")
+    from shapely.geometry import Polygon
 
-    area = area_of_polygon(*zip(*points))
-    result_x = 0
-    result_y = 0
-    N = len(points)
-    points = IT.cycle(points)
-    x1, y1 = next(points)
-    for i in range(N):
-        x0, y0 = x1, y1
-        x1, y1 = next(points)
-        cross = (x0 * y1) - (x1 * y0)
-        result_x += (x0 + x1) * cross
-        result_y += (y0 + y1) * cross
-    result_x /= area * 6.0
-    result_y /= area * 6.0
-    return (result_x, result_y)
+    pgon = Polygon(points)
+    return pgon.centroid.x, pgon.centroid.y
 
 
 class Point:
