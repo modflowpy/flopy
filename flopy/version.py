7a8a9852c9f7dcdeeae87c13b1ec97e745270cfc
wpbonelli <wbonelli@ucar.edu>
2024-08-08 14:30:37 -0400

    ci(release): update version to 3.9.0.dev0

--- flopy/version.py
+++ flopy/version.py
@@ -1,4 +1,4 @@
 # flopy version file automatically created using
-# update_version.py on August 08, 2024 13:58:49
+# update_version.py on August 08, 2024 14:29:26
 
-__version__ = "3.8.0"
+__version__ = "3.9.0.dev0"
