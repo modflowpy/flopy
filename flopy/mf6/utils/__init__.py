9c435e6685027023ed73679397279f6f5ef9e838
Bonelli <wbonelli@contractor.usgs.gov>
2026-02-25 18:19:23 -0500

    fix init file

--- flopy/mf6/utils/__init__.py
+++ flopy/mf6/utils/__init__.py
@@ -3,3 +3,4 @@ from .generate_classes import generate_classes
 from .lakpak_utils import get_lak_connections
 from .mfsimlistfile import MfSimulationList
 from .model_splitter import Mf6Splitter
+from .postprocessing import get_residuals, get_structured_faceflows
